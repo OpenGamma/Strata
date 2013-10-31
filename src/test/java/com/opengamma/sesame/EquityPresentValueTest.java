43d0b462fab60228152885a51b839b706e8f4d8c
Chris Kent <chris@opengamma.com>
2013-10-31 12:46:24 +0000

    SSM-9 - Fix after merge

--- src/test/java/com/opengamma/sesame/EquityPresentValueTest.java
+++ src/test/java/com/opengamma/sesame/EquityPresentValueTest.java
@@ -10,26 +10,23 @@ import static com.opengamma.sesame.ResultStatus.SUCCESS;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.opengamma.core.id.ExternalSchemes;
-import com.opengamma.core.value.MarketDataRequirementNames;
 import com.opengamma.financial.security.equity.EquitySecurity;
 import com.opengamma.util.money.Currency;
-import com.opengamma.util.tuple.Pair;
-import com.opengamma.util.tuple.Pairs;
 
 public class EquityPresentValueTest {
 
   EquityPresentValueFunction _equityPresentValueFunction;
+  MarketDataProvider _marketDataProviderFunction;
 
-  @BeforeTest
+
+  @BeforeMethod
   public void setUp() {
-    _equityPresentValueFunction = new EquityPresentValue();
+    _marketDataProviderFunction = new MarketDataProvider(new StandardResultGenerator());
+    _equityPresentValueFunction = new EquityPresentValue(_marketDataProviderFunction);
   }
 
   @Test
@@ -42,14 +39,8 @@ public class EquityPresentValueTest {
 
   @Test
   public void testMarketDataAvailable() {
-
     EquitySecurity security = new EquitySecurity("LSE", "LSE", "BloggsCo", Currency.GBP);
     security.setExternalIdBundle(ExternalSchemes.bloombergTickerSecurityId("BLGG").toBundle());
-
-    Map<MarketDataRequirement, Pair<MarketDataStatus, ? extends MarketDataValue>> marketData = new HashMap<>();
-    marketData.put(
-        StandardMarketDataRequirement.of(security, MarketDataRequirementNames.MARKET_VALUE),
-        Pairs.of(MarketDataStatus.AVAILABLE, new SingleMarketDataValue(123.45)));
 
     FunctionResult<Double> result = _equityPresentValueFunction.presentValue(security);
     assertThat(result.getStatus(), is(SUCCESS));
