52b951f8bb3919868ca0eba688e387ab5dbdf2bc
Chris Kent <chris@opengamma.com>
2013-11-13 09:16:01 +0000

    Comment

--- src/main/java/com/opengamma/sesame/trace/Tracer.java
+++ src/main/java/com/opengamma/sesame/trace/Tracer.java
@@ -14,6 +14,9 @@ import java.lang.reflect.Method;
  * FullTracer - records all calls with return values and args
  * AggregatingTracer - counts invocations to the same method. linked map impl
  * MergingTracer - merges adjacent invocations that are the same (e.g. loops)
+ *
+ * might need an incremental stateful version if we need to show live updating call graphs in the UI.
+ * need to assign a stable ID to nodes so the UI can keep track of node state as the graph is updated
  */
 /* package */ interface Tracer {
 
