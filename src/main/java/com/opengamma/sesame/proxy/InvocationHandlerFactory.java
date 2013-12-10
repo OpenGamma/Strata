664efd3350c5d73f95f36c30efe289b1ac6cd191
Chris Kent <chris@opengamma.com>
2013-12-10 17:34:56 +0000

    SSM-83 - Comments

--- src/main/java/com/opengamma/sesame/proxy/InvocationHandlerFactory.java
+++ src/main/java/com/opengamma/sesame/proxy/InvocationHandlerFactory.java
@@ -12,6 +12,10 @@ import java.lang.reflect.InvocationHandler;
  * This class is necessary because node decorators are invoked when the graph model is built but
  * the handler can't be created until the graph is built and the object being proxied is created. This interface
  * provides a level of indirection that separates the node decoration from handler creation.
+ * TODO this is fatally flawed. this is part of the node which means it must be serializable
+ * but that's never going to work for any non-trivial proxy which needs to hook into the system (e.g. caching)
+ * should the create() method take a ComponentMap?
+ * could the proxy just have a ref to the type of the factory and get one from the component map?
  */
 public interface InvocationHandlerFactory {
 
