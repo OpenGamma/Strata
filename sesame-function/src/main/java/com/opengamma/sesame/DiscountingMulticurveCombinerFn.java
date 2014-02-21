ec1008a46eb6551acbd1dc38194aecaa8aeb9e66
Stephen Colebourne <stephen@opengamma.com>
2014-02-21 16:28:20 +0000

    Checkstyle

--- sesame-function/src/main/java/com/opengamma/sesame/DiscountingMulticurveCombinerFn.java
+++ sesame-function/src/main/java/com/opengamma/sesame/DiscountingMulticurveCombinerFn.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.sesame;
 
 import com.opengamma.analytics.financial.forex.method.FXMatrix;
--- sesame-function/src/main/java/com/opengamma/sesame/ExposureFunctionsDiscountingMulticurveCombinerFn.java
+++ sesame-function/src/main/java/com/opengamma/sesame/ExposureFunctionsDiscountingMulticurveCombinerFn.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.sesame;
 
 import static com.opengamma.util.result.ResultGenerator.failure;
