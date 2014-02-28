510d581f9283c045db2d9fcdff1c1da36a543ca9
Jonathan Senior <jonathan@opengamma.com>
2014-02-28 11:04:30 +0000

    [SSM-150] Fixed historical implementation of MarketDataFnFactory. WIP - others to follow. (Tim)

---
+++ sesame-engine/src/main/java/com/opengamma/sesame/marketdata/FixedHistoricalMarketDataFnFactory.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.sesame.marketdata;
+
+import com.opengamma.core.config.ConfigSource;
+import com.opengamma.core.historicaltimeseries.HistoricalTimeSeriesSource;
+import com.opengamma.engine.marketdata.spec.FixedHistoricalMarketDataSpecification;
+import com.opengamma.engine.marketdata.spec.MarketDataSpecification;
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ * 
+ */
+public class FixedHistoricalMarketDataFnFactory implements MarketDataFnFactory {
+
+  private final ConfigSource _configSource;
+  private final HistoricalTimeSeriesSource _historicalTimeSeriesSource;
+  private final String _currencyMatrixConfigName;
+  private final String _dataSource;
+  private final String _dataProvider;
+  
+  public FixedHistoricalMarketDataFnFactory(ConfigSource configSource, HistoricalTimeSeriesSource historicalTimeSeriesSource, String currencyMatrixConfigName, String dataSource, String dataProvider) {
+    _configSource = ArgumentChecker.notNull(configSource, "configSource");
+    _historicalTimeSeriesSource = ArgumentChecker.notNull(historicalTimeSeriesSource, "historicalTimeSeriesSource");
+    _currencyMatrixConfigName = ArgumentChecker.notNull(currencyMatrixConfigName, "currencyMatrixConfigName");
+    _dataSource = dataSource;
+    _dataProvider = dataProvider;
+  }
+  
+  @Override
+  public MarketDataFn create(MarketDataSpecification spec) {
+    if (!(ArgumentChecker.notNull(spec, "spec") instanceof FixedHistoricalMarketDataSpecification)) {
+      throw new IllegalArgumentException("Expected " + FixedHistoricalMarketDataSpecification.class + " but was " + spec.getClass());
+    }
+    FixedHistoricalMarketDataSpecification fixedHistoricalMarketDataSpec = (FixedHistoricalMarketDataSpecification) spec;
+    RawMarketDataSource dataSource = new HistoricalRawMarketDataSource(_historicalTimeSeriesSource, fixedHistoricalMarketDataSpec.getSnapshotDate(), _dataSource, _dataProvider);
+    return new EagerMarketDataFn(dataSource, _configSource, _currencyMatrixConfigName);
+  }
+
+}
