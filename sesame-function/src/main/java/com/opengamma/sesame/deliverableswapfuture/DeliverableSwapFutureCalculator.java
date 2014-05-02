88c1514ef378d3c2cda891e0cae5f778d2328e91
mclornp <paulm@opengamma.com>
2014-05-02 10:20:40 +0100

    [SSM-295] Added support for PV01 calcs for DSFs

--- sesame-function/src/main/java/com/opengamma/sesame/deliverableswapfuture/DeliverableSwapFutureCalculator.java
+++ sesame-function/src/main/java/com/opengamma/sesame/deliverableswapfuture/DeliverableSwapFutureCalculator.java
@@ -5,7 +5,10 @@
  */
 package com.opengamma.sesame.deliverableswapfuture;
 
+import com.opengamma.analytics.util.amount.ReferenceAmount;
+import com.opengamma.util.money.Currency;
 import com.opengamma.util.result.Result;
+import com.opengamma.util.tuple.Pair;
 
 /**
  * Common interface for deliverable swap future calculators.
@@ -13,5 +16,7 @@ import com.opengamma.util.result.Result;
 public interface DeliverableSwapFutureCalculator {
 
   Result<Double> calculateSecurityModelPrice();
+  
+  Result<ReferenceAmount<Pair<String, Currency>>> calculatePV01();
 
 }
