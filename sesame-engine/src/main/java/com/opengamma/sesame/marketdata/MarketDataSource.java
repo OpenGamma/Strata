9d259818f6908c4d4a0c1dfc7f1054146fb9dfdc
Stephen Colebourne <stephen@opengamma.com>
2014-05-13 15:05:23 +0100

    SSM-317 - Enhance MapMarketDataSource
    Add convenient methods to create instances for testing
    Javadoc
    Test

--- sesame-engine/src/main/java/com/opengamma/sesame/marketdata/MarketDataSource.java
+++ sesame-engine/src/main/java/com/opengamma/sesame/marketdata/MarketDataSource.java
@@ -6,10 +6,12 @@
 package com.opengamma.sesame.marketdata;
 
 import com.opengamma.id.ExternalIdBundle;
+import com.opengamma.util.result.FailureStatus;
 import com.opengamma.util.result.Result;
 
 /**
  * A source of market data.
+ * <p>
  * This may be a source of live market data or backed by a database of historical data or snapshots.
  * This is intended to be a low level interface, functions are expected to use {@link MarketDataFn}.
  */
@@ -17,10 +19,14 @@ public interface MarketDataSource {
 
   /**
    * Returns an item of market data.
+   * <p>
+   * If the market data is not present, the result will be returned with a
+   * status of {@link FailureStatus#MISSING_DATA}.
    *
-   * @param id the ID of the data
-   * @param fieldName the name of the field in the market data record
-   * @return the data value
+   * @param id  the external identifier of the data
+   * @param fieldName  the name of the field in the market data record
+   * @return a success result of the market data value, or a failure result of {@code MISSING_DATA}
    */
   Result<?> get(ExternalIdBundle id, FieldName fieldName);
+
 }
--- sesame-function/src/test/java/com/opengamma/sesame/EquityPresentValueTest.java
+++ sesame-function/src/test/java/com/opengamma/sesame/EquityPresentValueTest.java
@@ -6,34 +6,22 @@
 package com.opengamma.sesame;
 
 import static com.opengamma.util.result.FailureStatus.MISSING_DATA;
-import static com.opengamma.util.result.FailureStatus.PENDING_DATA;
 import static com.opengamma.util.result.SuccessStatus.SUCCESS;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 
-import java.util.Collections;
-import java.util.Map;
-
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.threeten.bp.ZonedDateTime;
 
-import com.google.common.collect.ImmutableMap;
 import com.opengamma.core.id.ExternalSchemes;
-import com.opengamma.core.value.MarketDataRequirementNames;
-import com.opengamma.engine.marketdata.spec.MarketData;
 import com.opengamma.financial.currency.CurrencyMatrix;
 import com.opengamma.financial.security.equity.EquitySecurity;
-import com.opengamma.id.ExternalIdBundle;
 import com.opengamma.sesame.marketdata.DefaultMarketDataFn;
-import com.opengamma.sesame.marketdata.FieldName;
-import com.opengamma.sesame.marketdata.LDClient;
 import com.opengamma.sesame.marketdata.MapMarketDataSource;
 import com.opengamma.sesame.marketdata.MarketDataSource;
-import com.opengamma.sesame.marketdata.ResettableLiveMarketDataSource;
 import com.opengamma.util.money.Currency;
-import com.opengamma.util.result.FailureStatus;
 import com.opengamma.util.result.Result;
 import com.opengamma.util.result.ResultStatus;
 
@@ -48,37 +36,22 @@ public class EquityPresentValueTest {
 
   @Test
   public void testMarketDataUnavailable() {
-
     EquitySecurity security = new EquitySecurity("LSE", "LSE", "BloggsCo", Currency.GBP);
     security.setExternalIdBundle(ExternalSchemes.bloombergTickerSecurityId("BLGG").toBundle());
-    Map<ExternalIdBundle, Double> marketData = Collections.emptyMap();
-    Environment env = new SimpleEnvironment(ZonedDateTime.now(), marketDataSource(marketData));
+    Environment env = new SimpleEnvironment(ZonedDateTime.now(), MapMarketDataSource.of());
     Result<Double> result = _equityPresentValueFn.presentValue(env, security);
     assertThat(result.getStatus(), is((ResultStatus) MISSING_DATA));
   }
 
   @Test
   public void testMarketDataAvailable() {
-
     EquitySecurity security = new EquitySecurity("LSE", "LSE", "BloggsCo", Currency.GBP);
     security.setExternalIdBundle(ExternalSchemes.bloombergTickerSecurityId("BLGG").toBundle());
-
-    //Map<MarketDataRequirement, MarketDataItem> marketData = new HashMap<>();
-    //MarketDataRequirement requirement = MarketDataRequirementFactory.of(security, MarketDataRequirementNames.MARKET_VALUE);
-    //marketData.put(requirement, MarketDataItem.available(123.45));
-    Map<ExternalIdBundle, Double> marketData = ImmutableMap.of(security.getExternalIdBundle(), 123.45);
-    MarketDataSource dataSource = marketDataSource(marketData);
+    MarketDataSource dataSource = MapMarketDataSource.of(security.getExternalIdBundle(), 123.45);
     Environment env = new SimpleEnvironment(ZonedDateTime.now(), dataSource);
     Result<Double> result = _equityPresentValueFn.presentValue(env, security);
     assertThat(result.getStatus(), is((ResultStatus) SUCCESS));
     assertThat(result.getValue(), is(123.45));
   }
 
-  private MarketDataSource marketDataSource(Map<ExternalIdBundle, Double> marketData) {
-    MapMarketDataSource.Builder builder = MapMarketDataSource.builder();
-    for (Map.Entry<ExternalIdBundle, Double> entry : marketData.entrySet()) {
-      builder.add(entry.getKey(), entry.getValue());
-    }
-    return builder.build();
-  }
 }
