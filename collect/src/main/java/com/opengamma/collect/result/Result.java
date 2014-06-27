633a5501f4426bfa691a1b8dfc825b01b07b8f6a
Stephen Colebourne <stephen@opengamma.com>
2014-06-27 17:58:17 +0100

    PLT-348 - Add package javadoc

--- collect/src/main/java/com/opengamma/collect/result/Result.java
+++ collect/src/main/java/com/opengamma/collect/result/Result.java
@@ -43,7 +43,7 @@ import com.opengamma.collect.validate.ArgChecker;
  * <p>
  * The result model is typically used in a subsystem following functional programming style.
  * Functions will be written to have {@code Result<T>} as the return type.
- * Result failures will be instead of exceptions.
+ * Instead of using exceptions, code will return failure results.
  * <p>
  * Results can be generated using the factory methods on this class.
  *
