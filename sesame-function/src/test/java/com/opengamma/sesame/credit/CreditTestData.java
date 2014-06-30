696c07e48d8ae638acaf65a99f3fedb9ed54e318
traggattog <tim@opengamma.com>
2014-06-30 14:01:06 +0100

    [PLT-341] Formatted to reduce line length

--- sesame-function/src/test/java/com/opengamma/sesame/credit/CreditTestData.java
+++ sesame-function/src/test/java/com/opengamma/sesame/credit/CreditTestData.java
@@ -30,8 +30,48 @@ public final class CreditTestData {
   private static final LocalDate VALUATION_DATE = LocalDate.of(2014, 3, 27);
 
   private static int MONEY_MARKET_INSTS = 6;
-  private static List<Period> TENORS = ImmutableList.of(Period.ofMonths(1), Period.ofMonths(2), Period.ofMonths(3), Period.ofMonths(6), Period.ofMonths(9), Period.ofYears(1), Period.ofYears(2), Period.ofYears(3), Period.ofYears(4), Period.ofYears(5), Period.ofYears(6), Period.ofYears(7), Period.ofYears(8), Period.ofYears(9), Period.ofYears(10), Period.ofYears(11), Period.ofYears(12), Period.ofYears(15), Period.ofYears(20), Period.ofYears(25), Period.ofYears(30));
-  private static List<Double> RATES = ImmutableList.of(0.00445, 0.009488, 0.012337, 0.017762, 0.01935, 0.020838, 0.01652, 0.02018, 0.023033, 0.02525, 0.02696, 0.02825, 0.02931, 0.03017, 0.03092, 0.0316, 0.03231, 0.03367, 0.03419, 0.03411, 0.03412);
+  private static List<Period> TENORS = ImmutableList.of(Period.ofMonths(1), 
+                                                        Period.ofMonths(2), 
+                                                        Period.ofMonths(3), 
+                                                        Period.ofMonths(6), 
+                                                        Period.ofMonths(9), 
+                                                        Period.ofYears(1), 
+                                                        Period.ofYears(2), 
+                                                        Period.ofYears(3), 
+                                                        Period.ofYears(4), 
+                                                        Period.ofYears(5), 
+                                                        Period.ofYears(6), 
+                                                        Period.ofYears(7), 
+                                                        Period.ofYears(8), 
+                                                        Period.ofYears(9), 
+                                                        Period.ofYears(10), 
+                                                        Period.ofYears(11), 
+                                                        Period.ofYears(12), 
+                                                        Period.ofYears(15), 
+                                                        Period.ofYears(20), 
+                                                        Period.ofYears(25), 
+                                                        Period.ofYears(30));
+  private static List<Double> RATES = ImmutableList.of(0.00445,
+                                                       0.009488,
+                                                       0.012337,
+                                                       0.017762,
+                                                       0.01935,
+                                                       0.020838,
+                                                       0.01652,
+                                                       0.02018,
+                                                       0.023033,
+                                                       0.02525,
+                                                       0.02696,
+                                                       0.02825,
+                                                       0.02931,
+                                                       0.03017,
+                                                       0.03092,
+                                                       0.0316,
+                                                       0.03231,
+                                                       0.03367,
+                                                       0.03419,
+                                                       0.03411,
+                                                       0.03412);
 
   private CreditTestData() {}
   
