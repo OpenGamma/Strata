f041b15cf65d4d8d5e5499e30f65841a256354d3
traggattog <tim@opengamma.com>
2014-07-03 13:50:31 +0100

    [PLT-352] Added Test annotation

--- sesame-function/src/test/java/com/opengamma/sesame/credit/market/DefaultCreditKeyMapperFnTest.java
+++ sesame-function/src/test/java/com/opengamma/sesame/credit/market/DefaultCreditKeyMapperFnTest.java
@@ -13,10 +13,12 @@ import com.opengamma.financial.analytics.isda.credit.CreditCurveDataKey;
 import com.opengamma.financial.analytics.isda.credit.config.CreditCurveDataKeyMap;
 import com.opengamma.util.money.Currency;
 import com.opengamma.util.result.Result;
+import com.opengamma.util.test.TestGroup;
 
 /**
  * Tests use cases of {@link DefaultCreditKeyMapperFn}.
  */
+@Test(groups = TestGroup.UNIT)
 public class DefaultCreditKeyMapperFnTest {
   
   private CreditCurveDataKey _source;
