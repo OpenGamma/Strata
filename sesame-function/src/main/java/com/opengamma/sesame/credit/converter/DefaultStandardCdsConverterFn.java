707c494a195d4ab6f6e443f0d404b11c266b8545
traggattog <tim@opengamma.com>
2014-07-16 12:56:48 +0100

    [PLT-352] Added handling for change in credit curve convention - calendars now referenced by region ids.

--- sesame-function/src/main/java/com/opengamma/sesame/credit/converter/DefaultStandardCdsConverterFn.java
+++ sesame-function/src/main/java/com/opengamma/sesame/credit/converter/DefaultStandardCdsConverterFn.java
@@ -7,9 +7,15 @@ package com.opengamma.sesame.credit.converter;
 
 import com.opengamma.analytics.financial.credit.isdastandardmodel.CDSAnalytic;
 import com.opengamma.analytics.financial.credit.isdastandardmodel.CDSAnalyticFactory;
+import com.opengamma.core.holiday.HolidaySource;
+import com.opengamma.core.region.RegionSource;
+import com.opengamma.financial.analytics.conversion.CalendarUtils;
 import com.opengamma.financial.analytics.isda.credit.CreditCurveData;
 import com.opengamma.financial.convention.IsdaCreditCurveConvention;
+import com.opengamma.financial.convention.calendar.Calendar;
+import com.opengamma.financial.convention.calendar.MondayToFridayCalendar;
 import com.opengamma.financial.security.credit.StandardCDSSecurity;
+import com.opengamma.id.ExternalId;
 import com.opengamma.sesame.Environment;
 import com.opengamma.sesame.credit.IsdaCreditCurve;
 import com.opengamma.util.result.Result;
@@ -19,13 +25,29 @@ import com.opengamma.util.result.Result;
  */
 public class DefaultStandardCdsConverterFn implements StandardCdsConverterFn {
 
+  private final RegionSource _regionSource;
+  private final HolidaySource _holidaySource;
+  
+  public DefaultStandardCdsConverterFn(RegionSource regionSource, HolidaySource holidaySource) {
+    _regionSource = regionSource;
+    _holidaySource = holidaySource;
+  }
+
   @Override
   public Result<CDSAnalytic> toCdsAnalytic(Environment env, StandardCDSSecurity cds, IsdaCreditCurve curve) {
     CreditCurveData curveData = curve.getCurveData();
     IsdaCreditCurveConvention convention = curveData.getCurveConventionLink().resolve();
+    ExternalId regionId = convention.getRegionId();
+    Calendar calendar;
+    if (regionId == null) {
+      calendar = new MondayToFridayCalendar("weekday calendar");
+    } else {
+      calendar = CalendarUtils.getCalendar(_regionSource, _holidaySource, regionId);
+    }
+    
     CDSAnalyticFactory factory = new CDSAnalyticFactory()
                                       .with(convention.getBusinessDayConvention())
-                                      .with(convention.getRegionCalendar())
+                                      .with(calendar)
                                       .with(convention.getCouponInterval())
                                       .with(convention.getStubType())
                                       .withAccrualDCC(convention.getAccrualDayCount())
