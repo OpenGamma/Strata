b1d5e8a323dc5878582270ad3fad591e545064eb
Stephen Colebourne <stephen@opengamma.com>
2014-07-31 11:52:03 +0100

    REQS-382 - Enhance Tenor
    Allow it to be added to a date directly
    Test coverage

--- basics/src/main/java/com/opengamma/basics/date/Tenor.java
+++ basics/src/main/java/com/opengamma/basics/date/Tenor.java
@@ -6,7 +6,15 @@
 package com.opengamma.basics.date;
 
 import java.io.Serializable;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.util.List;
 
 import org.joda.convert.FromString;
 import org.joda.convert.ToString;
@@ -14,10 +22,15 @@ import org.joda.convert.ToString;
 import com.opengamma.collect.ArgChecker;
 
 /**
- * A tenor indicating how long it will take for a financial
- * instrument to reach maturity.
+ * A tenor indicating how long it will take for a financial instrument to reach maturity.
+ * <p>
+ * A tenor is allowed to be any non-negative period of days, weeks, month or years.
+ * This class provides constants for common tenors which are best used by static import.
+ * <p>
+ * {@code Tenor} implements {@code TemporalAmount} allowing it to be directly added to a date.
  */
-public class Tenor implements Serializable {
+public class Tenor
+    implements TemporalAmount, Serializable {
 
   /**
    * Serialization version.
@@ -28,172 +41,138 @@ public class Tenor implements Serializable {
    * A tenor of one day.
    */
   public static final Tenor TENOR_1D = ofDays(1);
-
   /**
    * A tenor of two days.
    */
   public static final Tenor TENOR_2D = ofDays(2);
-
   /**
    * A tenor of three days.
    */
   public static final Tenor TENOR_3D = ofDays(3);
-
   /**
    * A tenor of 1 week.
    */
-  public static final Tenor TENOR_1W= ofWeeks(1);
-
+  public static final Tenor TENOR_1W = ofWeeks(1);
   /**
    * A tenor of 2 weeks.
    */
   public static final Tenor TENOR_2W = ofWeeks(2);
-
   /**
    * A tenor of 3 weeks.
    */
   public static final Tenor TENOR_3W = ofWeeks(3);
-
   /**
    * A tenor of 6 weeks.
    */
   public static final Tenor TENOR_6W = ofWeeks(6);
-
   /**
    * A tenor of 1 month.
    */
   public static final Tenor TENOR_1M = ofMonths(1);
-
   /**
    * A tenor of 2 months.
    */
   public static final Tenor TENOR_2M = ofMonths(2);
-
   /**
    * A tenor of 3 months.
    */
   public static final Tenor TENOR_3M = ofMonths(3);
-
   /**
    * A tenor of 4 months.
    */
   public static final Tenor TENOR_4M = ofMonths(4);
-
   /**
    * A tenor of 5 months.
    */
   public static final Tenor TENOR_5M = ofMonths(5);
-
   /**
    * A tenor of 6 months.
    */
   public static final Tenor TENOR_6M = ofMonths(6);
-
   /**
    * A tenor of 7 months.
    */
   public static final Tenor TENOR_7M = ofMonths(7);
-
   /**
    * A tenor of 8 months.
    */
   public static final Tenor TENOR_8M = ofMonths(8);
-
   /**
    * A tenor of 9 months.
    */
   public static final Tenor TENOR_9M = ofMonths(9);
-
   /**
    * A tenor of 10 months.
    */
   public static final Tenor TENOR_10M = ofMonths(10);
-
   /**
    * A tenor of 11 months.
    */
   public static final Tenor TENOR_11M = ofMonths(11);
-
   /**
    * A tenor of 12 months.
    */
   public static final Tenor TENOR_12M = ofMonths(12);
-
   /**
    * A tenor of 18 months.
    */
   public static final Tenor TENOR_18M = ofMonths(18);
-
   /**
    * A tenor of 1 year.
    */
   public static final Tenor TENOR_1Y = ofYears(1);
-
   /**
    * A tenor of 2 years.
    */
   public static final Tenor TENOR_2Y = ofYears(2);
-
   /**
    * A tenor of 3 years.
    */
   public static final Tenor TENOR_3Y = ofYears(3);
-
   /**
    * A tenor of 4 years.
    */
   public static final Tenor TENOR_4Y = ofYears(4);
-
   /**
    * A tenor of 5 years.
    */
   public static final Tenor TENOR_5Y = ofYears(5);
-
   /**
    * A tenor of 6 years.
    */
   public static final Tenor TENOR_6Y = ofYears(6);
-
   /**
    * A tenor of 7 years.
    */
   public static final Tenor TENOR_7Y = ofYears(7);
-
   /**
    * A tenor of 8 years.
    */
   public static final Tenor TENOR_8Y = ofYears(8);
-
   /**
    * A tenor of 9 years.
    */
   public static final Tenor TENOR_9Y = ofYears(9);
-
   /**
    * A tenor of 10 years.
    */
   public static final Tenor TENOR_10Y = ofYears(10);
-
   /**
    * A tenor of 12 years.
    */
   public static final Tenor TENOR_12Y = ofYears(12);
-
   /**
    * A tenor of 15 years.
    */
   public static final Tenor TENOR_15Y = ofYears(15);
-
   /**
    * A tenor of 20 years.
    */
   public static final Tenor TENOR_20Y = ofYears(20);
-
   /**
    * A tenor of 25 years.
    */
   public static final Tenor TENOR_25Y = ofYears(25);
-
   /**
    * A tenor of 30 years.
    */
@@ -204,11 +183,13 @@ public class Tenor implements Serializable {
    */
   private final Period period;
 
+  //-------------------------------------------------------------------------
   /**
    * Obtains a {@code Tenor} from a {@code Period}.
    *
    * @param period  the period to convert to a tenor
    * @return the tenor
+   * @throws IllegalArgumentException if the period is negative
    */
   public static Tenor of(Period period) {
     return new Tenor(period);
@@ -217,8 +198,9 @@ public class Tenor implements Serializable {
   /**
    * Returns a tenor backed by a period of days.
    *
-   * @param days The number of days
-   * @return The tenor
+   * @param days  the number of days
+   * @return the tenor
+   * @throws IllegalArgumentException if the period is negative
    */
   public static Tenor ofDays(int days) {
     return of(Period.ofDays(days));
@@ -227,8 +209,9 @@ public class Tenor implements Serializable {
   /**
    * Returns a tenor backed by a period of weeks.
    *
-   * @param weeks The number of weeks
-   * @return The tenor
+   * @param weeks  the number of weeks
+   * @return the tenor
+   * @throws IllegalArgumentException if the period is negative
    */
   public static Tenor ofWeeks(int weeks) {
     return of(Period.ofDays(weeks * 7));
@@ -237,8 +220,9 @@ public class Tenor implements Serializable {
   /**
    * Returns a tenor backed by a period of months.
    *
-   * @param months The number of months
-   * @return The tenor
+   * @param months  the number of months
+   * @return the tenor
+   * @throws IllegalArgumentException if the period is negative
    */
   public static Tenor ofMonths(int months) {
     return of(Period.ofMonths(months));
@@ -247,13 +231,15 @@ public class Tenor implements Serializable {
   /**
    * Returns a tenor backed by a period of years.
    *
-   * @param years The number of years
-   * @return The tenor
+   * @param years  the number of years
+   * @return the tenor
+   * @throws IllegalArgumentException if the period is negative
    */
   public static Tenor ofYears(int years) {
     return of(Period.ofYears(years));
   }
 
+  //-------------------------------------------------------------------------
   /**
    * Parses a formatted string representing the tenor.
    * <p>
@@ -262,14 +248,20 @@ public class Tenor implements Serializable {
    *
    * @param toParse  the string representing the tenor
    * @return the tenor
+   * @throws IllegalArgumentException if the tenor cannot be parsed
    */
   @FromString
   public static Tenor parse(String toParse) {
     ArgChecker.notNull(toParse, "toParse");
     String prefixed = toParse.startsWith("P") ? toParse : "P" + toParse;
-    return new Tenor(Period.parse(prefixed));
+    try {
+      return new Tenor(Period.parse(prefixed));
+    } catch (DateTimeParseException ex) {
+      throw new IllegalArgumentException(ex);
+    }
   }
 
+  //-------------------------------------------------------------------------
   /**
    * Creates a tenor.
    *
@@ -281,6 +273,7 @@ public class Tenor implements Serializable {
     this.period = period;
   }
 
+  //-------------------------------------------------------------------------
   /**
    * Gets the tenor period.
    *
@@ -290,14 +283,90 @@ public class Tenor implements Serializable {
     return period;
   }
 
+  //-------------------------------------------------------------------------
+  /**
+   * Gets the value of the specified unit.
+   * <p>
+   * This will return a value for the years, months and days units.
+   * Note that weeks are not included.
+   * All other units throw an exception.
+   *
+   * @param unit  the unit to query
+   * @return the value of the unit
+   * @throws UnsupportedTemporalTypeException if the unit is not supported
+   */
   @Override
-  public boolean equals(Object o) {
-    if (o == null || !(o instanceof Tenor)) {
-      return false;
-    }
-    return period.equals(((Tenor) o).period);
+  public long get(TemporalUnit unit) {
+    return period.get(unit);
   }
 
+  /**
+   * Gets the units supported by a tenor.
+   * <p>
+   * This returns a list containing years, months and days.
+   * Note that weeks are not included.
+   *
+   * @return a list containing the years, months and days units, not null
+   */
+  @Override
+  public List<TemporalUnit> getUnits() {
+    return period.getUnits();
+  }
+
+  /**
+   * Adds this tenor to the specified date.
+   * <p>
+   * This is an implementation method used by {@link LocalDate#plus(TemporalAmount)}.
+   * See {@link Period#addTo(Temporal)} for more details.
+   *
+   * @param temporal  the temporal object to add to, not null
+   * @return the result with this tenor added, not null
+   * @throws DateTimeException if unable to add
+   * @throws ArithmeticException if numeric overflow occurs
+   */
+  @Override
+  public Temporal addTo(Temporal temporal) {
+    return period.addTo(temporal);
+  }
+
+  /**
+   * Subtracts this tenor from the specified date.
+   * <p>
+   * This is an implementation method used by {@link LocalDate#minus(TemporalAmount)}.
+   * See {@link Period#subtractFrom(Temporal)} for more details.
+   *
+   * @param temporal  the temporal object to subtract from, not null
+   * @return the result with this tenor subtracted, not null
+   * @throws DateTimeException if unable to subtract
+   * @throws ArithmeticException if numeric overflow occurs
+   */
+  @Override
+  public Temporal subtractFrom(Temporal temporal) {
+    return period.subtractFrom(temporal);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Checks if this tenor equals another tenor.
+   * <p>
+   * The comparison checks the tenor period.
+   * 
+   * @param obj  the other tenor, null returns false
+   * @return true if equal
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || !(obj instanceof Tenor)) {
+      return false;
+    }
+    return period.equals(((Tenor) obj).period);
+  }
+
+  /**
+   * Returns a suitable hash code for the currency.
+   * 
+   * @return the hash code
+   */
   @Override
   public int hashCode() {
     return period.hashCode();
--- basics/src/test/java/com/opengamma/basics/date/TenorTest.java
+++ basics/src/test/java/com/opengamma/basics/date/TenorTest.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.basics.date;
 
+import static com.opengamma.basics.date.Tenor.TENOR_10M;
 import static com.opengamma.basics.date.Tenor.TENOR_12M;
 import static com.opengamma.basics.date.Tenor.TENOR_18M;
 import static com.opengamma.basics.date.Tenor.TENOR_1D;
@@ -22,70 +23,147 @@ import static com.opengamma.basics.date.Tenor.TENOR_3Y;
 import static com.opengamma.basics.date.Tenor.TENOR_4M;
 import static com.opengamma.basics.date.Tenor.TENOR_4Y;
 import static com.opengamma.basics.date.Tenor.TENOR_6W;
+import static com.opengamma.collect.TestHelper.assertJodaConvert;
+import static com.opengamma.collect.TestHelper.assertSerialization;
+import static com.opengamma.collect.TestHelper.assertThrows;
+import static java.time.temporal.ChronoUnit.CENTURIES;
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.MONTHS;
+import static java.time.temporal.ChronoUnit.YEARS;
 import static org.testng.Assert.assertEquals;
 
+import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeParseException;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * Tests for the tenor class.
  */
+@Test
 public class TenorTest {
 
-  @Test
-  public void testPeriodStaticConstructor() {
+  public void test_of_Period() {
     assertEquals(Tenor.of(Period.ofDays(1)), TENOR_1D);
     assertEquals(Tenor.of(Period.ofDays(7)), TENOR_1W);
+    assertEquals(Tenor.of(Period.ofWeeks(2)), TENOR_2W);
     assertEquals(Tenor.of(Period.ofMonths(1)), TENOR_1M);
     assertEquals(Tenor.of(Period.ofYears(1)), TENOR_1Y);
+  }
+
+  public void test_of_int() {
     assertEquals(Tenor.ofDays(1), TENOR_1D);
     assertEquals(Tenor.ofDays(7), TENOR_1W);
+    assertEquals(Tenor.ofWeeks(2), TENOR_2W);
     assertEquals(Tenor.ofMonths(1), TENOR_1M);
     assertEquals(Tenor.ofYears(1), TENOR_1Y);
   }
 
-  @Test(expectedExceptions = DateTimeParseException.class)
-  public void testParseFailure() {
-    Tenor.parse("2K");
+  public void test_of_notNegative() {
+    assertThrows(() -> Tenor.of(Period.ofDays(-1)), IllegalArgumentException.class);
+    assertThrows(() -> Tenor.ofDays(-1), IllegalArgumentException.class);
+    assertThrows(() -> Tenor.ofWeeks(-1), IllegalArgumentException.class);
+    assertThrows(() -> Tenor.ofMonths(-1), IllegalArgumentException.class);
+    assertThrows(() -> Tenor.ofYears(-1), IllegalArgumentException.class);
   }
 
-  @Test(expectedExceptions = IllegalArgumentException.class)
-  public void testNegativePeriodNotAllowed() {
-    Tenor.ofDays(-1);
+  //-------------------------------------------------------------------------
+  public void test_parse_String_roundTrip() {
+    assertEquals(Tenor.parse(TENOR_10M.toString()), TENOR_10M);
   }
 
-  @Test
-  public void testGetPeriod() {
+  @DataProvider(name = "parseGood")
+  Object[][] data_parseGood() {
+    return new Object[][] {
+        {"2D", TENOR_2D},
+        {"2W", TENOR_2W},
+        {"6W", TENOR_6W},
+        {"2M", TENOR_2M},
+        {"12M", TENOR_12M},
+        {"1Y", TENOR_1Y},
+        {"2Y", TENOR_2Y},
+        {"P2D", TENOR_2D},
+        {"P2W", TENOR_2W},
+        {"P6W", TENOR_6W},
+        {"P2M", TENOR_2M},
+        {"P12M", TENOR_12M},
+        {"P1Y", TENOR_1Y},
+        {"P2Y", TENOR_2Y},
+    };
+  }
+
+  @Test(dataProvider = "parseGood")
+  public void test_parse_String_good(String input, Tenor expected) {
+    assertEquals(Tenor.parse(input), expected);
+  }
+
+  @DataProvider(name = "parseBad")
+  Object[][] data_parseBad() {
+    return new Object[][] {
+      {""},
+      {"2"},
+      {"2K"},
+      {"-2D"},
+      {null},
+    };
+  }
+
+  @Test(dataProvider = "parseBad", expectedExceptions = IllegalArgumentException.class)
+  public void test_parse_String_bad(String input) {
+    Tenor.parse(input);
+  }
+
+  //-------------------------------------------------------------------------
+  public void test_getPeriod() {
     assertEquals(TENOR_3D.getPeriod(), Period.ofDays(3));
     assertEquals(TENOR_3W.getPeriod(), Period.ofDays(21));
     assertEquals(TENOR_3M.getPeriod(), Period.ofMonths(3));
     assertEquals(TENOR_3Y.getPeriod(), Period.ofYears(3));
+    assertThrows(() -> TENOR_10M.get(CENTURIES), UnsupportedTemporalTypeException.class);
   }
 
-  @Test
-  public void testParsePeriod() {
-    assertEquals(Tenor.parse("2D"), TENOR_2D);
-    assertEquals(Tenor.parse("2W"), TENOR_2W);
-    assertEquals(Tenor.parse("6W"), TENOR_6W);
-    assertEquals(Tenor.parse("2M"), TENOR_2M);
-    assertEquals(Tenor.parse("1Y"), TENOR_1Y);
-    assertEquals(Tenor.parse("12M"), TENOR_12M);
-    assertEquals(Tenor.parse("18M"), TENOR_18M);
-    assertEquals(Tenor.parse("2Y"), TENOR_2Y);
-  }
-  @Test
-  public void testParsePrefixedPeriod() {
-    assertEquals(Tenor.parse("P2D"), TENOR_2D);
-    assertEquals(Tenor.parse("P2W"), TENOR_2W);
-    assertEquals(Tenor.parse("P6W"), TENOR_6W);
-    assertEquals(Tenor.parse("P2M"), TENOR_2M);
-    assertEquals(Tenor.parse("P2Y"), TENOR_2Y);
+  //-------------------------------------------------------------------------
+  public void test_temporalAmount() {
+    assertEquals(TENOR_3D.getUnits(), ImmutableList.of(YEARS, MONTHS, DAYS));
+    assertEquals(TENOR_3D.get(DAYS), 3);
+    assertEquals(TENOR_3D.addTo(LocalDate.of(2014, 6, 30)), LocalDate.of(2014, 7, 3));
+    assertEquals(TENOR_3D.subtractFrom(LocalDate.of(2014, 6, 30)), LocalDate.of(2014, 6, 27));
+    assertEquals(LocalDate.of(2014, 6, 30).plus(TENOR_1W), LocalDate.of(2014, 7, 7));
+    assertEquals(LocalDate.of(2014, 6, 30).minus(TENOR_1W), LocalDate.of(2014, 6, 23));
   }
 
-  @Test
-  public void testToString() {
+  //-------------------------------------------------------------------------
+  public void test_equals_hashCode() {
+    Tenor a1 = TENOR_3D;
+    Tenor a2 = Tenor.ofDays(3);
+    Tenor b = TENOR_4M;
+    assertEquals(a1.equals(a1), true);
+    assertEquals(a1.equals(b), false);
+    assertEquals(a1.equals(a2), true);
+    
+    assertEquals(a2.equals(a1), true);
+    assertEquals(a2.equals(a2), true);
+    assertEquals(a2.equals(b), false);
+    
+    assertEquals(b.equals(a1), false);
+    assertEquals(b.equals(a2), false);
+    assertEquals(b.equals(b), true);
+    
+    assertEquals(a1.hashCode(), a2.hashCode());
+  }
+
+  public void test_equals_bad() {
+    assertEquals(TENOR_3D.equals(null), false);
+    assertEquals(TENOR_3D.equals("String"), false);
+    assertEquals(TENOR_3D.equals(new Object()), false);
+  }
+
+  //-------------------------------------------------------------------------
+  public void test_toString() {
     assertEquals(TENOR_3D.toString(), "3D");
     assertEquals(TENOR_2W.toString(), "2W");
     assertEquals(TENOR_4M.toString(), "4M");
@@ -93,6 +171,19 @@ public class TenorTest {
     assertEquals(TENOR_12M.toString(), "12M");
     assertEquals(TENOR_18M.toString(), "18M");
     assertEquals(TENOR_4Y.toString(), "4Y");
+  }
+
+  //-----------------------------------------------------------------------
+  public void test_serialization() {
+    assertSerialization(TENOR_3D);
+    assertSerialization(TENOR_4M);
+    assertSerialization(TENOR_3Y);
+  }
+
+  public void test_jodaConvert() {
+    assertJodaConvert(Tenor.class, TENOR_3D);
+    assertJodaConvert(Tenor.class, TENOR_4M);
+    assertJodaConvert(Tenor.class, TENOR_3Y);
   }
 
 }
