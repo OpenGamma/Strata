97a6b24d59cfad8e0257a81ab31a3ae1ffecb86b
Julian Howarth <julian@opengamma.com>
2014-09-01 16:55:31 +0100

    REQS-373 - Rough initial version of curve calibration

--- analytics-api/src/main/java/com/opengamma/platform/analytics/YieldCurve.java
+++ analytics-api/src/main/java/com/opengamma/platform/analytics/YieldCurve.java
@@ -16,15 +16,42 @@ import com.opengamma.basics.date.Tenor;
  */
 public interface YieldCurve {
 
+  /**
+   * Get the analytics calibrated curve.
+   *
+   * @return the calibrated curve
+   */
   YieldAndDiscountCurve getCalibratedCurve();
 
+  /**
+   * Gets the discount factor from the calibrated curve for a
+   * particular date.
+   *
+   * @param date  the date to get discount factor for
+   * @return the discount factor for the date
+   */
   double getDiscountFactor(LocalDate date);
 
+  /**
+   * Gets the discount factor from the calibrated curve for a
+   * particular tenor.
+   *
+   * @param tenor  the tenor to get discount factor for
+   * @return the discount factor for the tenor
+   */
   double getDiscountFactor(Tenor tenor);
 
+  /**
+   * Get the forward rate from the calibrated curve between two tenors.
+   *
+   * @param startTenor  the start tenor
+   * @param endTenor  the end tenor
+   * @return the forward rate
+   */
+  // TODO - validate if it makes any sense for start to be after end
   double getForwardRate(Tenor startTenor, Tenor endTenor);
 
-  // shift methods ... can shift underlying data or built curve
+  // TODO - shift methods ... can shift underlying data or built curve
 
-  // access initial market data ?
+  // TODO - access the market data which was used to construct the curve
 }
