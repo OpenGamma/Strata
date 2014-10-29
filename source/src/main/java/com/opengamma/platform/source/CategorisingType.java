d6cb4fcd35489b6bdd006c0102da1b6ea2781c6f
Julian Howarth <julian@opengamma.com>
2014-10-29 14:33:58 +0000

    PLT-471 - SearchableSource and associated classes
    - address further review comments

--- source/src/main/java/com/opengamma/platform/source/CategorisingType.java
+++ source/src/main/java/com/opengamma/platform/source/CategorisingType.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.platform.source;
 
 import java.lang.annotation.ElementType;
