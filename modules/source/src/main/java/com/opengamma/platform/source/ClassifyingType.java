009b06668215e3209a186330d9d1c5a07f8d5547
Julian Howarth <julian@opengamma.com>
2014-11-13 22:23:11 +0000

    PLT-486 - New master
    Identify and store bean classifiers

--- modules/source/src/main/java/com/opengamma/platform/source/ClassifyingType.java
+++ modules/source/src/main/java/com/opengamma/platform/source/ClassifyingType.java
@@ -20,5 +20,5 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface CategorisingType {
+public @interface ClassifyingType {
 }
