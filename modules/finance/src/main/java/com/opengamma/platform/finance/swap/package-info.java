2a0b86a4a7f8d7c80b4ba08358611e49c5925084
Stephen Colebourne <stephen@opengamma.com>
2014-12-11 15:45:21 +0000

    PLT-533 - Swap domain model
    A domain model for swaps
    Phase 1, the trade, swap and swap leg

---
+++ modules/finance/src/main/java/com/opengamma/platform/finance/swap/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+
+/**
+ * Entity objects describing an interest rate swap.
+ * <p>
+ * A swap takes place between two counterparties who agree to exchange streams of payments.
+ * In the simplest vanilla swap, there are two legs, one with a fixed rate and the other a floating rate.
+ * Many other more complex swaps can also be represented.
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.opengamma.platform.finance.swap;
