0979e24301e2bbc5997cddba098353901c605611
Stephen Colebourne <stephen@opengamma.com>
2015-01-07 13:31:48 +0000

    PLT-533 Swap domain model
    Better definition of compounding

--- modules/finance/src/main/java/com/opengamma/platform/finance/swap/CompoundingMethod.java
+++ modules/finance/src/main/java/com/opengamma/platform/finance/swap/CompoundingMethod.java
@@ -41,10 +41,10 @@ public enum CompoundingMethod {
   /**
    * Flat compounding applies.
    * <p>
-   * Compounding is based for each period on the total of the observed rate and the spread
-   * for the interest on the notional, known as the <i>Basic Compounding Period Amount</i>,
-   * but only the observed rate (and not the spread) for the interest on previous periods
-   * interest, known as the <i>Additional Compounding Period Amount</i>.
+   * For interest on the notional, known as the <i>Basic Compounding Period Amount</i>,
+   * compounding is based on the total of the observed rate and the spread.
+   * For interest on previously accrued interest, known as the <i>Additional Compounding Period Amount</i>,
+   * compounding is based only on the observed rate, excluding the spread.
    * <p>
    * Defined as "Flat Compounding" in the ISDA 2006 definitions.
    */
