c847156385d005a759f847a9923cd72e263199a1
Stephen Colebourne <stephen@opengamma.com>
2015-01-08 17:15:52 +0000

    PLT-578 IborRateObservation implementation
    Remove meaningless super

--- modules/pricer-impl/src/main/java/com/opengamma/platform/pricer/impl/observation/ForwardIborRateObservationFn.java
+++ modules/pricer-impl/src/main/java/com/opengamma/platform/pricer/impl/observation/ForwardIborRateObservationFn.java
@@ -28,7 +28,6 @@ public class ForwardIborRateObservationFn
    * Creates an instance.
    */
   public ForwardIborRateObservationFn() {
-    super();
   }
 
   //-------------------------------------------------------------------------
