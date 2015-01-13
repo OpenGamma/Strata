dca6a969db8cbeefde4dc2489d838c7f1c7bcbe0
Stephen Colebourne <stephen@opengamma.com>
2015-01-13 16:39:01 +0000

    PLT-590 MultiCurrencyAmount.collector
    Fix indentation

--- modules/basics/src/main/java/com/opengamma/basics/currency/MultiCurrencyAmount.java
+++ modules/basics/src/main/java/com/opengamma/basics/currency/MultiCurrencyAmount.java
@@ -146,18 +146,18 @@ public final class MultiCurrencyAmount
    */
   public static Collector<CurrencyAmount, ?, MultiCurrencyAmount> collector() {
     return Collector.<CurrencyAmount, Map<Currency, CurrencyAmount>, MultiCurrencyAmount>of(
-            // accumulate into a map
-            HashMap::new,
-            // merge two CurrencyAmounts if same currency
-            (map, ca) -> map.merge(ArgChecker.notNull(ca, "amount").getCurrency(), ca, CurrencyAmount::plus),
-            // combine two maps
-            (map1, map2) -> {
-              map2.values().forEach((ca2) -> map1.merge(ca2.getCurrency(), ca2, CurrencyAmount::plus));
-              return map1;
-            },
-            // convert to MultiCurrencyAmount
-            map -> new MultiCurrencyAmount(ImmutableSortedSet.copyOf(map.values())),
-            UNORDERED);
+        // accumulate into a map
+        HashMap::new,
+        // merge two CurrencyAmounts if same currency
+        (map, ca) -> map.merge(ArgChecker.notNull(ca, "amount").getCurrency(), ca, CurrencyAmount::plus),
+        // combine two maps
+        (map1, map2) -> {
+          map2.values().forEach((ca2) -> map1.merge(ca2.getCurrency(), ca2, CurrencyAmount::plus));
+          return map1;
+        },
+        // convert to MultiCurrencyAmount
+        map -> new MultiCurrencyAmount(ImmutableSortedSet.copyOf(map.values())),
+        UNORDERED);
   }
 
   /**
