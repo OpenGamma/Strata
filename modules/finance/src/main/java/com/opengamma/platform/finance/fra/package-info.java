2bb2d1bea7eddffa8a473306d14ee04819b45060
Stephen Colebourne <stephen@opengamma.com>
2015-02-01 23:25:46 +0000

    PLT-576 FRA trade model
    Trade model for forward rate agreements (FRAs)

---
+++ modules/finance/src/main/java/com/opengamma/platform/finance/fra/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+
+/**
+ * Entity objects describing a forward rate agreement (FRA).
+ * <p>
+ * A FRA takes place between two counterparties who agree to exchange
+ * the difference between a fixed and floating rate of interest at a future date.
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.opengamma.platform.finance.fra;
+
---
+++ modules/finance/src/test/java/com/opengamma/platform/finance/fra/FraTradeTest.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.platform.finance.fra;
+
+import static com.opengamma.basics.BuySell.BUY;
+import static com.opengamma.basics.date.BusinessDayConventions.MODIFIED_FOLLOWING;
+import static com.opengamma.basics.date.HolidayCalendars.GBLO;
+import static com.opengamma.basics.index.IborIndices.GBP_LIBOR_3M;
+import static com.opengamma.collect.TestHelper.assertSerialization;
+import static com.opengamma.collect.TestHelper.coverBeanEquals;
+import static com.opengamma.collect.TestHelper.coverImmutableBean;
+import static com.opengamma.collect.TestHelper.date;
+import static org.testng.Assert.assertEquals;
+
+import java.time.LocalDate;
+
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.opengamma.basics.date.AdjustableDate;
+import com.opengamma.basics.date.BusinessDayAdjustment;
+import com.opengamma.basics.date.DaysAdjustment;
+import com.opengamma.collect.id.StandardId;
+import com.opengamma.platform.finance.TradeType;
+
+/**
+ * Test.
+ */
+@Test
+public class FraTradeTest {
+
+  private static final double NOTIONAL_1M = 1_000_000d;
+  private static final double NOTIONAL_2M = 2_000_000d;
+  private static final BusinessDayAdjustment BDA_MOD_FOLLOW = BusinessDayAdjustment.of(MODIFIED_FOLLOWING, GBLO);
+  private static final DaysAdjustment MINUS_TWO_DAYS = DaysAdjustment.ofBusinessDays(-2, GBLO);
+  private static final Fra FRA = Fra.builder()
+      .buySell(BUY)
+      .paymentDate(AdjustableDate.of(date(2015, 6, 16), BDA_MOD_FOLLOW))
+      .startDate(date(2015, 6, 15))
+      .endDate(date(2015, 9, 15))
+      .fixedRate(0.25d)
+      .index(GBP_LIBOR_3M)
+      .fixingOffset(MINUS_TWO_DAYS)
+      .notional(NOTIONAL_1M)
+      .build();
+
+  //-------------------------------------------------------------------------
+  public void test_builder() {
+    FraTrade test = FraTrade.builder()
+        .standardId(StandardId.of("OG-Trade", "1"))
+        .tradeDate(LocalDate.of(2014, 12, 3))
+        .fra(FRA)
+        .build();
+    assertEquals(test.getTradeType(), TradeType.of("Fra"));
+  }
+
+  //-------------------------------------------------------------------------
+  public void coverage() {
+    FraTrade test = FraTrade.builder()
+        .standardId(StandardId.of("OG-Trade", "1"))
+        .tradeDate(LocalDate.of(2014, 12, 3))
+        .fra(FRA)
+        .build();
+    coverImmutableBean(test);
+    FraTrade test2 = FraTrade.builder()
+        .standardId(StandardId.of("OG-Trade", "2"))
+        .attributes(ImmutableMap.of("key", "value"))
+        .tradeDate(LocalDate.of(2014, 12, 5))
+        .fra(FRA.toBuilder().notional(NOTIONAL_2M).build())
+        .build();
+    coverBeanEquals(test, test2);
+  }
+
+  public void test_serialization() {
+    FraTrade test = FraTrade.builder()
+        .standardId(StandardId.of("OG-Trade", "1"))
+        .tradeDate(LocalDate.of(2014, 12, 3))
+        .fra(FRA)
+        .build();
+    assertSerialization(test);
+  }
+
+}
