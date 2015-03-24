e3dca70297cd59be22e4375c54be566fcc004b82
Stephen Colebourne <stephen@opengamma.com>
2015-03-24 17:31:36 +0000

    PLT-689 Collect all indices referred to by a swap
    Simplify algorithm

--- modules/finance/src/main/java/com/opengamma/platform/finance/rate/swap/Swap.java
+++ modules/finance/src/main/java/com/opengamma/platform/finance/rate/swap/Swap.java
@@ -80,13 +80,10 @@ public final class Swap
    */
   public boolean isCrossCurrency() {
     // optimized for performance
-    int size = legs.size();
-    if (size > 1) {
-      Currency firstCurrency = legs.get(0).getCurrency();
-      for (int i = 1; i < size; i++) {
-        if (!legs.get(i).getCurrency().equals(firstCurrency)) {
-          return true;
-        }
+    Currency firstCurrency = legs.get(0).getCurrency();
+    for (int i = 1; i < legs.size(); i++) {
+      if (!legs.get(i).getCurrency().equals(firstCurrency)) {
+        return true;
       }
     }
     return false;
