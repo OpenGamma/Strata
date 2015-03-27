b6f2fbaef35a1e47c9ff37449147743a9cd71969
Chris Kent <chris@opengamma.com>
2015-03-27 12:39:07 +0000

    STR-51 - Address review comment

--- modules/collect/src/main/java/com/opengamma/collect/result/Result.java
+++ modules/collect/src/main/java/com/opengamma/collect/result/Result.java
@@ -349,11 +349,7 @@ public final class Result<T>
    * @return a success result if the value is non-null, else a failure result
    */
   public static <R> Result<R> ofNullable(@Nullable R value) {
-    if (value != null) {
-      return success(value);
-    } else {
-      return failure(FailureReason.MISSING_DATA, "Found null where a value was expected");
-    }
+    return ofNullable(value, FailureReason.MISSING_DATA, "Found null where a value was expected");
   }
 
   //-------------------------------------------------------------------------
