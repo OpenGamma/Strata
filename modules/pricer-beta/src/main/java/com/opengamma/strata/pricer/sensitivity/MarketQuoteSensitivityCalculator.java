f10afff6ab97b505653aa593acb24f0448869288
marc-opengamma <marc@opengamma.com>
2015-04-27 12:45:20 +0100

    Sensitivity. Created a calculator to transform parameter (zero-coupon) curve sensitivity to Market Quotes (par rate) sensitivities.

---
+++ modules/pricer-beta/src/main/java/com/opengamma/strata/pricer/sensitivity/MarketQuoteSensitivityCalculator.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.pricer.sensitivity;
+
+import java.util.Map.Entry;
+
+import com.google.common.collect.ImmutableMap;
+import com.opengamma.analytics.financial.provider.curve.CurveBuildingBlock;
+import com.opengamma.analytics.financial.provider.curve.CurveBuildingBlockBundle;
+import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
+import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
+import com.opengamma.analytics.math.matrix.MatrixAlgebra;
+import com.opengamma.analytics.math.matrix.OGMatrixAlgebra;
+import com.opengamma.strata.collect.ArgChecker;
+import com.opengamma.strata.collect.tuple.Pair;
+
+/**
+ * Calculator to obtain the Market Quote sensitivities from the {@link CurveBuildingBlockBundle} obtained through the 
+ * curve calibration.
+ * <p>
+ * The Market Quote sensitivities are also called Par Rate when the instruments used in the curve calibration are
+ * quoted in rate, e.g. IRS, FRA or OIS.
+ */
+public class MarketQuoteSensitivityCalculator {
+  
+  /** The matrix algebra used for matrix inversion. */
+  private static final MatrixAlgebra MATRIX_ALGEBRA = new OGMatrixAlgebra();
+  //TODO: The method internally uses DoubleMatrix1D and DoubleMatrix2D. This should be reviewed.
+  
+  public CurveParameterSensitivity fromParameterSensitivity(CurveParameterSensitivity parameterSensitivity,
+      CurveBuildingBlockBundle blocks) {
+    ArgChecker.notNull(parameterSensitivity, "Sensitivity");
+    ArgChecker.notNull(blocks, "Units");
+    ImmutableMap<SensitivityKey, double[]> sensitivityMap = parameterSensitivity.getSensitivities();
+    CurveParameterSensitivity result = CurveParameterSensitivity.empty();
+    for(Entry<SensitivityKey, double[]> entry: sensitivityMap.entrySet()) {
+      SensitivityKey key = entry.getKey();
+      ArgChecker.isTrue((key instanceof NameCurrencySensitivityKey) || 
+          (key instanceof NameSensitivityKey), "only Sensitivity keys with curve name supported");
+      // TODO: improve the "instanceof"
+      String curveName = null;
+      if(key instanceof NameCurrencySensitivityKey) {
+        curveName = ((NameCurrencySensitivityKey) key).getCurveName();
+      } else if(key instanceof NameSensitivityKey){
+        curveName = ((NameSensitivityKey) key).getCurveName();        
+      }
+      Pair<CurveBuildingBlock, DoubleMatrix2D> blockEntry = blocks.getBlock(curveName);
+      double[] parameterSensitivityEntry = entry.getValue();
+      DoubleMatrix1D parameterSensitivityEntryMatrix = new DoubleMatrix1D(parameterSensitivityEntry);
+      double[] keySensi = ((DoubleMatrix1D) MATRIX_ALGEBRA
+          .multiply(parameterSensitivityEntryMatrix, blockEntry.getSecond())).getData();
+      // split between different curves
+      for (final String name2 : blockEntry.getFirst().getAllNames()) {
+        int nbParameters = blockEntry.getFirst().getNbParameters(name2);
+        int start = blockEntry.getFirst().getStart(name2);
+        double[] sensiName2 = new double[nbParameters];
+        System.arraycopy(keySensi, start, sensiName2, 0, nbParameters);
+        SensitivityKey key2 = null;
+        if(key instanceof NameCurrencySensitivityKey) {
+          NameCurrencySensitivityKey keyName = (NameCurrencySensitivityKey) key;
+          key2 = NameCurrencySensitivityKey.of(name2, keyName.getCurrency());
+        } else if(key instanceof NameSensitivityKey){
+          key2 = NameSensitivityKey.of(name2);        
+        }
+        result.combinedWith(key2, sensiName2);        
+      }
+    }
+    return result;
+  }
+
+}
