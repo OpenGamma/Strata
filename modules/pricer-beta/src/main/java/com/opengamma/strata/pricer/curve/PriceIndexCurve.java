d21eac490231e629fad7b2add94ade68b3eebdd6
marc-opengamma <marc@opengamma.com>
2015-05-07 19:15:50 +0100

    Beta: Created first draft for PriceIndexCurve (inflation).

---
+++ modules/pricer-beta/src/main/java/com/opengamma/strata/pricer/curve/PriceIndexCurve.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.pricer.curve;
+
+import java.time.YearMonth;
+
+import com.opengamma.strata.basics.index.PriceIndex;
+
+/**
+ * Curve providing forward {@link PriceIndex} estimates for inflation related products.
+ */
+public interface PriceIndexCurve {
+  
+  /**
+   * Returns the curve's name.
+   * @return the name.
+   */
+  String getName();
+
+  /**
+   * Returns the estimated price index value for the given month.
+   * @param month  the month
+   * @return the price index value
+   */
+  double getPriceIndex(YearMonth month);
+
+  /**
+   * Returns the sensitivities of the price index to the curve parameters at a given month.
+   * 
+   * @param time The time
+   * @return The sensitivities. If the time is less than 1e<sup>-6</sup>, the rate is
+   * ill-defined and zero is returned.
+   */
+  Double[] getPriceIndexParameterSensitivity(YearMonth month);
+  // TODO: should this be a double[], Double[] or something else.
+  
+  /**
+   * Returns the number of parameters defining the curve.
+   * @return The number of parameters
+   */
+  int getNumberOfParameters();  
+  
+  /**
+   * Returns a new curve for which each of the parameters has been shift according to a vector of shifts.
+   * @param shifts  the parameters shifts
+   * @return the new curve
+   */
+  PriceIndexCurve shiftCurve(double[] shifts);
+  
+  
+  
+}
