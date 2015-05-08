15777e3e519fea6982a6db22802b79edeecb0b63
Chris Kent <chris@opengamma.com>
2015-05-08 16:13:07 +0100

    Basic curve filters and perturbations

---
+++ modules/engine/src/test/java/com/opengamma/strata/engine/marketdata/scenarios/curves/CurveRateIndexFilterTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.engine.marketdata.scenarios.curves;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.analytics.financial.model.interestrate.curve.YieldCurve;
+import com.opengamma.analytics.math.curve.ConstantDoublesCurve;
+import com.opengamma.strata.basics.index.IborIndices;
+import com.opengamma.strata.marketdata.id.RateIndexCurveId;
+
+@Test
+public class CurveRateIndexFilterTest {
+
+  public void match() {
+    CurveRateIndexFilter filter = CurveRateIndexFilter.of(IborIndices.USD_LIBOR_1M);
+    RateIndexCurveId id = RateIndexCurveId.of(IborIndices.USD_LIBOR_1M, "curveName");
+    YieldCurve curve = YieldCurve.from(ConstantDoublesCurve.from(1d, "curveName"));
+    assertThat(filter.apply(id, curve)).isTrue();
+  }
+
+  public void noMatch() {
+    CurveRateIndexFilter filter = CurveRateIndexFilter.of(IborIndices.USD_LIBOR_1M);
+    RateIndexCurveId id = RateIndexCurveId.of(IborIndices.USD_LIBOR_3M, "curveName");
+    YieldCurve curve = YieldCurve.from(ConstantDoublesCurve.from(1d, "curveName"));
+    assertThat(filter.apply(id, curve)).isFalse();
+  }
+}
