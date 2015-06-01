c180678735b7b8c51a011026d68076af5c3897f9
Chris Kent <chris@opengamma.com>
2015-06-01 11:25:54 +0100

    Address review comments

--- modules/market/src/test/java/com/opengamma/strata/market/curve/ParallelShiftedCurveTest.java
+++ modules/market/src/test/java/com/opengamma/strata/market/curve/ParallelShiftedCurveTest.java
@@ -10,6 +10,8 @@ import static org.assertj.core.api.Assertions.offset;
 
 import org.testng.annotations.Test;
 
+import com.opengamma.analytics.math.interpolation.Interpolator1DFactory;
+
 @Test
 public class ParallelShiftedCurveTest {
 
@@ -25,5 +27,20 @@ public class ParallelShiftedCurveTest {
     Curve shiftedCurve = ParallelShiftedCurve.relative(curve, 0.1);
     assertThat(shiftedCurve.yValue(0)).isEqualTo(3.3, offset(1e-10));
     assertThat(shiftedCurve.yValue(1)).isEqualTo(3.3, offset(1e-10));
+  }
+
+  public void firstDerivative() {
+    InterpolatedNodalCurve curve = InterpolatedNodalCurve.of(
+        "curve",
+        new double[]{0, 1},
+        new double[]{2, 2.5},
+        Interpolator1DFactory.LINEAR_INSTANCE); // TODO Use CurveInterpolators.LINEAR when #261 is fixed
+
+    Curve absoluteShiftedCurve = ParallelShiftedCurve.absolute(curve, 1);
+    Curve relativeShiftedCurve = ParallelShiftedCurve.relative(curve, 0.2);
+
+    assertThat(curve.firstDerivative(0.1)).isEqualTo(0.5);
+    assertThat(absoluteShiftedCurve.firstDerivative(0.1)).isEqualTo(0.5);
+    assertThat(relativeShiftedCurve.firstDerivative(0.1)).isEqualTo(0.5 * 1.2);
   }
 }
