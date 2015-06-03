aa658702f2ffd05b2d9ee347f44ce79bb7ef7c3c
Jonathan Senior <jonathan@opengamma.com>
2015-06-03 10:39:10 +0100

    Fixing function return type

--- modules/engine/src/main/java/com/opengamma/strata/engine/config/MissingConfigCalculationFunction.java
+++ modules/engine/src/main/java/com/opengamma/strata/engine/config/MissingConfigCalculationFunction.java
@@ -7,7 +7,6 @@ package com.opengamma.strata.engine.config;
 
 import com.opengamma.strata.basics.CalculationTarget;
 import com.opengamma.strata.collect.Messages;
-import com.opengamma.strata.collect.result.Result;
 import com.opengamma.strata.engine.calculations.function.CalculationSingleFunction;
 import com.opengamma.strata.engine.marketdata.CalculationMarketData;
 import com.opengamma.strata.engine.marketdata.CalculationRequirements;
@@ -15,7 +14,7 @@ import com.opengamma.strata.engine.marketdata.CalculationRequirements;
 /**
  * Function used when there is no function registered that can calculate a requested value.
  */
-public class MissingConfigCalculationFunction implements CalculationSingleFunction<CalculationTarget, Object> {
+public class MissingConfigCalculationFunction implements CalculationSingleFunction<CalculationTarget, Void> {
 
   @Override
   public CalculationRequirements requirements(CalculationTarget target) {
@@ -23,7 +22,7 @@ public class MissingConfigCalculationFunction implements CalculationSingleFuncti
   }
 
   @Override
-  public Result<Object> execute(CalculationTarget target, CalculationMarketData marketData) {
+  public Void execute(CalculationTarget target, CalculationMarketData marketData) {
     // TODO Pass in the measure and include it in the error message
     throw new IllegalStateException(Messages.format("No configuration found to calculate a value for {}", target));
   }
