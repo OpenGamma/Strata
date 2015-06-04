fe40fa9445ec4b74a273765ecff6674ecff6ca83
Chris Kent <chris@opengamma.com>
2015-06-04 10:46:17 +0100

    Add requirements for FX rates used in automatic currency conversion

--- modules/engine/src/main/java/com/opengamma/strata/engine/calculations/CalculationTask.java
+++ modules/engine/src/main/java/com/opengamma/strata/engine/calculations/CalculationTask.java
@@ -5,10 +5,16 @@
  */
 package com.opengamma.strata.engine.calculations;
 
+import static com.opengamma.strata.collect.Guavate.toImmutableList;
+
+import java.util.List;
 import java.util.Optional;
 
 import com.opengamma.strata.basics.CalculationTarget;
 import com.opengamma.strata.basics.currency.Currency;
+import com.opengamma.strata.basics.currency.CurrencyPair;
+import com.opengamma.strata.basics.market.FxRateKey;
+import com.opengamma.strata.basics.market.MarketDataId;
 import com.opengamma.strata.basics.market.MarketDataKey;
 import com.opengamma.strata.collect.ArgChecker;
 import com.opengamma.strata.collect.result.FailureReason;
@@ -96,6 +102,25 @@ public class CalculationTask {
 
     for (MarketDataKey<?> key : calculationRequirements.getSingleValueRequirements()) {
       requirementsBuilder.addValues(marketDataMappings.getIdForKey(key));
+    }
+    Optional<Currency> optionalReportingCurrency = reportingRules.reportingCurrency(target);
+
+    if (optionalReportingCurrency.isPresent()) {
+      Currency reportingCurrency = optionalReportingCurrency.get();
+
+      // Add requirements for the FX rates needed to convert the output values into the reporting currency
+      // TODO This assumes there is a conventional pair for the combination.
+      //   If the pair has been created on the fly that isn't necessarily the case.
+      //   We need a consistent way across the whole system to construct pairs that have no market convention.
+      //   This means the pair created here will be the same pair used in the conversion code.
+      // TODO Change this when #283 is addressed. Hopefully simplify and don't worry about market convention pairs.
+      List<MarketDataId<Double>> fxRateIds = calculationRequirements.getOutputCurrencies().stream()
+          .map(outputCurrency -> CurrencyPair.of(outputCurrency, reportingCurrency))
+          .map(pair -> pair.isConventional() ? pair : pair.inverse())
+          .map(FxRateKey::of)
+          .map(marketDataMappings::getIdForKey)
+          .collect(toImmutableList());
+      requirementsBuilder.addValues(fxRateIds);
     }
     return requirementsBuilder.build();
   }
