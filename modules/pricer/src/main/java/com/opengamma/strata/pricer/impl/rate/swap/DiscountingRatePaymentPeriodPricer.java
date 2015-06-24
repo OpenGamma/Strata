e595c6fa512698ecdc461cbe1e16a8128ee34740
Jonathan Senior <jonathan@opengamma.com>
2015-06-24 15:36:50 +0100

    Remove future value from explain map of accrual period

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/impl/rate/swap/DiscountingRatePaymentPeriodPricer.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/impl/rate/swap/DiscountingRatePaymentPeriodPricer.java
@@ -362,7 +362,11 @@ public class DiscountingRatePaymentPeriodPricer
         accrualPeriod.getRateObservation(), accrualPeriod.getStartDate(), accrualPeriod.getEndDate(), provider, builder);
     double payOffRate = rawRate * accrualPeriod.getGearing() + accrualPeriod.getSpread();
     double ua = unitNotionalAccrual(accrualPeriod, accrualPeriod.getSpread(), provider);
-    double fv = ua * notional;
+
+    // Note that the future value is not published since this is potentially misleading when
+    // compounding is being applied, and when it isn't then it's the same as the future
+    // value of the payment period.
+    
     builder.put(ExplainKey.ENTRY_TYPE, "AccrualPeriod");
     builder.put(ExplainKey.START_DATE, accrualPeriod.getStartDate());
     builder.put(ExplainKey.UNADJUSTED_START_DATE, accrualPeriod.getUnadjustedStartDate());
@@ -374,7 +378,6 @@ public class DiscountingRatePaymentPeriodPricer
     builder.put(ExplainKey.SPREAD, accrualPeriod.getSpread());
     builder.put(ExplainKey.PAY_OFF_RATE, accrualPeriod.getNegativeRateMethod().adjust(payOffRate));
     builder.put(ExplainKey.UNIT_AMOUNT, ua);
-    builder.put(ExplainKey.FUTURE_VALUE, CurrencyAmount.of(currency, fv));
   }
 
 }
