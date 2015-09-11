f58a6c291f798b2a6d2eb1bca61bdbdaf0f297c8
Stephen Colebourne <stephen@opengamma.com>
2015-09-11 14:43:26 +0100

    Add package info files

---
+++ modules/market/src/main/java/com/opengamma/strata/market/explain/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Support for explaining results.
+ * <p>
+ * This package provides functionality to allow calculated results to be explained.
+ * For example, the calculation of Present Value on an Interest Rate Swap has many different
+ * possible execution paths, and the explain functionality allows these to be understood.
+ */
+package com.opengamma.strata.market.explain;
+
---
+++ modules/market/src/main/java/com/opengamma/strata/market/value/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Package containing values of market data.
+ * <p>
+ * Each type in this package stores or defines a kind of market data.
+ * The generic term "value" is used for the package, however individual types prefer to
+ * use more specific names, such as "discount factors" or "Ibor rates".
+ * <p>
+ * Each type of value is associated with a key, found in the {@code com.opengamma.strata.market.key} package.
+ */
+package com.opengamma.strata.market.value;
+
