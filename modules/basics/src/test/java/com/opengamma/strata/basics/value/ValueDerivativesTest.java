c5ba3575256b5dbf6867e6c98db70c8615596f62
Marc Henrard <marc@opengamma.com>
2015-10-12 13:08:58 +0100

    Added ValueDerivatives object to store a value and its derivatives. Refactor the Balck and Normal formula to use the object.

---
+++ modules/basics/src/test/java/com/opengamma/strata/basics/value/ValueDerivativesTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.basics.value;
+
+import static com.opengamma.strata.collect.TestHelper.assertSerialization;
+import static com.opengamma.strata.collect.TestHelper.coverBeanEquals;
+import static com.opengamma.strata.collect.TestHelper.coverImmutableBean;
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.testng.annotations.Test;
+import org.testng.internal.junit.ArrayAsserts;
+
+/**
+ * Test {@link ValueDerivatives}.
+ */
+@Test
+public class ValueDerivativesTest {
+
+  private static final double VALUE = 123.4;
+  private static final double[] DERIVATIVES = {1.0, 2.0, 3.0};
+  
+  public void test_of() {
+    ValueDerivatives test = ValueDerivatives.of(VALUE, DERIVATIVES);
+    assertEquals(test.getValue(), VALUE, 0);
+    ArrayAsserts.assertArrayEquals(test.getDerivatives(), DERIVATIVES, 0);    
+  }
+
+  //-------------------------------------------------------------------------
+  public void coverage() {
+    ValueDerivatives test = ValueDerivatives.of(VALUE, DERIVATIVES);
+    coverImmutableBean(test);
+    ValueDerivatives test2 = ValueDerivatives.of(123.4, new double[] {1.0, 2.0, 3.0});
+    coverBeanEquals(test, test2);
+  }
+
+  public void test_serialization() {
+    ValueDerivatives test = ValueDerivatives.of(VALUE, DERIVATIVES);
+    assertSerialization(test);
+  }
+}
