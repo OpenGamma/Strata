dab08b452c5b33e5bd80e54009529d466bfc48bd
Stephen Colebourne <stephen@opengamma.com>
2015-11-19 16:24:20 +0000

    Fix package info

---
+++ modules/function/src/main/java/com/opengamma/strata/function/marketdata/mapping/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Mappings between market data keys and IDs.
+ */
+package com.opengamma.strata.function.marketdata.mapping;
---
+++ modules/market/src/main/java/com/opengamma/strata/market/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Data structures for market data.
+ */
+package com.opengamma.strata.market;
+
