7da77ed47b003a3876b150c625fe3a3a5bc18f6e
Chris Kent <chris@opengamma.com>
2015-12-01 10:21:49 +0000

    Fix equals and hashCode to allow for maps with array values

--- modules/calc/src/test/java/com/opengamma/strata/calc/runner/function/result/MultiCurrencyValuesArrayTest.java
+++ modules/calc/src/test/java/com/opengamma/strata/calc/runner/function/result/MultiCurrencyValuesArrayTest.java
@@ -158,6 +158,29 @@ public class MultiCurrencyValuesArrayTest {
     }
   }
 
+  /**
+   * Test the hand-written equals and hashCode methods which correctly handle maps with array values
+   */
+  public void equalsHashCode() {
+    MultiCurrencyValuesArray array =
+        MultiCurrencyValuesArray.of(
+            ImmutableList.of(
+                MultiCurrencyAmount.of(
+                    CurrencyAmount.of(Currency.GBP, 20),
+                    CurrencyAmount.of(Currency.USD, 30),
+                    CurrencyAmount.of(Currency.EUR, 40)),
+                MultiCurrencyAmount.of(
+                    CurrencyAmount.of(Currency.GBP, 21),
+                    CurrencyAmount.of(Currency.USD, 32),
+                    CurrencyAmount.of(Currency.EUR, 43)),
+                MultiCurrencyAmount.of(
+                    CurrencyAmount.of(Currency.GBP, 22),
+                    CurrencyAmount.of(Currency.USD, 33),
+                    CurrencyAmount.of(Currency.EUR, 44))));
+    assertThat(array).isEqualTo(VALUES_ARRAY);
+    assertThat(array.hashCode()).isEqualTo(VALUES_ARRAY.hashCode());
+  }
+
   public void getCurrencies() {
     assertThat(VALUES_ARRAY.getCurrencies()).isEqualTo(ImmutableSet.of(Currency.GBP, Currency.USD, Currency.EUR));
   }
