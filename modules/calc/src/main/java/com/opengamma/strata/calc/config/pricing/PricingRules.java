a756b66a8889c1d6aa796e274bc10e02ececcb22
Chris Kent <chris@opengamma.com>
2016-02-10 09:52:11 +0000

    Factory method to create PricingRules from PricingRule...

--- modules/calc/src/main/java/com/opengamma/strata/calc/config/pricing/PricingRules.java
+++ modules/calc/src/main/java/com/opengamma/strata/calc/config/pricing/PricingRules.java
@@ -5,8 +5,10 @@
  */
 package com.opengamma.strata.calc.config.pricing;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.opengamma.strata.basics.CalculationTarget;
 import com.opengamma.strata.calc.config.Measure;
@@ -21,21 +23,23 @@ import com.opengamma.strata.calc.config.Measure;
 public interface PricingRules {
 
   /**
-   * Returns a rule set that tries each of the specified rule sets in turn
-   * and returns the first valid configuration it finds.
+   * Obtains an instance based on the specified individual rules.
    *
-   * @param rules  the rule sets
-   * @return a rule set that tries each of the rule sets in turn and returns the first valid configuration it finds
+   * @param rules  the individual rules
+   * @return the combined rules
    */
-  public static PricingRules of(PricingRules... rules) {
-    switch (rules.length) {
-      case 0:
-        return PricingRules.empty();
-      case 1:
-        return rules[0];
-      default:
-        return CompositePricingRules.of(rules);
-    }
+  public static PricingRules of(PricingRule<?>... rules) {
+    return DefaultPricingRules.of(ImmutableList.copyOf(rules));
+  }
+
+  /**
+   * Obtains an instance based on the specified individual rules.
+   *
+   * @param rules  the individual rules
+   * @return the combined rules
+   */
+  public static PricingRules of(List<PricingRule<?>> rules) {
+    return DefaultPricingRules.of(rules);
   }
 
   /**
