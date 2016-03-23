a50dfbbe4e6cb6b8d30d8d4dadc985a9753abea2
Marc Henrard <marc@opengamma.com>
2016-03-23 15:53:00 +0000

    Add triangulation on counter currency.

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/calibration/MarketDataFxRateProvider.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/calibration/MarketDataFxRateProvider.java
@@ -40,14 +40,29 @@ class MarketDataFxRateProvider implements FxRateProvider {
     if (rate.isPresent()) {
       return rate.get().fxRate(baseCurrency, counterCurrency);
     }
-    // Try triangulation on first currency
-    Currency triangularCcy = baseCurrency.getTriangulationCurrency();
-    Optional<FxRate> rate1 = marketData.findValue(FxRateKey.of(baseCurrency, triangularCcy));
-    Optional<FxRate> rate2 = marketData.findValue(FxRateKey.of(triangularCcy, counterCurrency));
-    if(rate1.isPresent() && rate2.isPresent()) {
-      return rate1.get().crossRate(rate2.get()).fxRate(baseCurrency, counterCurrency);
+    // Try triangulation on base currency
+    Currency triangularBaseCcy = baseCurrency.getTriangulationCurrency();
+    Optional<FxRate> rateBase1 = marketData.findValue(FxRateKey.of(baseCurrency, triangularBaseCcy));
+    Optional<FxRate> rateBase2 = marketData.findValue(FxRateKey.of(triangularBaseCcy, counterCurrency));
+    if (rateBase1.isPresent() && rateBase2.isPresent()) {
+      return rateBase1.get().crossRate(rateBase2.get()).fxRate(baseCurrency, counterCurrency);
+    }
+    // Try triangulation on counter currency
+    Currency triangularCounterCcy = counterCurrency.getTriangulationCurrency();
+    Optional<FxRate> rateCounter1 = marketData.findValue(FxRateKey.of(baseCurrency, triangularCounterCcy));
+    Optional<FxRate> rateCounter2 = marketData.findValue(FxRateKey.of(triangularCounterCcy, counterCurrency));
+    if (rateCounter1.isPresent() && rateCounter2.isPresent()) {
+      return rateCounter1.get().crossRate(rateCounter2.get()).fxRate(baseCurrency, counterCurrency);
+    }
+    // Double triangulation
+    if(rateBase1.isPresent() && rateCounter2.isPresent()) {
+      Optional<FxRate> rateTriangular2 = marketData.findValue(FxRateKey.of(triangularBaseCcy, triangularCounterCcy));
+      if(rateTriangular2.isPresent()) {
+        return rateBase1.get().crossRate(rateTriangular2.get()).crossRate(rateCounter2.get())
+            .fxRate(baseCurrency, counterCurrency);        
+      }
     }
     throw new IllegalArgumentException("No market data available for pair " + baseCurrency + "/" + counterCurrency);
   }
-  
+
 }
