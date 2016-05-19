aa2315daf0d7412a8cacecd9d6644fd4a66ae008
Stephen Colebourne <stephen@opengamma.com>
2016-05-19 11:40:52 +0100

    Enhance package Javadoc

--- modules/calc/src/main/java/com/opengamma/strata/calc/runner/package-info.java
+++ modules/calc/src/main/java/com/opengamma/strata/calc/runner/package-info.java
@@ -5,7 +5,17 @@
  */
 
 /**
- * Types used when running calculations.
+ * The calculation runner.
+ * <p>
+ * This package includes the implementation of the calculation runner, which allows
+ * a grid of results to be calculated for a list of trades and columns.
+ * The primary classes for interacting with the calculation runner are in the
+ * {@code com.opengamma.strata.calc} package.
+ * <p>
+ * The calculations are performed by implementations of
+ * {@link com.opengamma.strata.calc.runner.CalculationFunction CalculationFunction}.
+ * Each calculation may be customized and controlled by an implementation of
+ * {@link com.opengamma.strata.calc.runner.CalculationParameter CalculationParameter}.
  */
 package com.opengamma.strata.calc.runner;
 
