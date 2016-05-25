1ed0f3abb2422792a9af0e4f14d4c84b04cfcc84
Stephen Colebourne <stephen@opengamma.com>
2016-05-25 10:37:53 +0100

    Add general parameter sensitivity objects
    Intended to replace the curve/surface specific ones

--- modules/market/src/main/java/com/opengamma/strata/market/curve/CurveName.java
+++ modules/market/src/main/java/com/opengamma/strata/market/curve/CurveName.java
@@ -5,18 +5,27 @@
  */
 package com.opengamma.strata.market.curve;
 
+import java.io.Serializable;
+
 import org.joda.convert.FromString;
 
-import com.opengamma.strata.collect.TypedString;
+import com.opengamma.strata.collect.ArgChecker;
+import com.opengamma.strata.market.MarketDataName;
 
 /**
  * The name of a curve.
  */
 public final class CurveName
-    extends TypedString<CurveName> {
+    extends MarketDataName<Curve>
+    implements Serializable {
 
   /** Serialization version. */
   private static final long serialVersionUID = 1L;
+
+  /**
+   * The name.
+   */
+  private final String name;
 
   //-------------------------------------------------------------------------
   /**
@@ -38,7 +47,18 @@ public final class CurveName
    * @param name  the name of the curve
    */
   private CurveName(String name) {
-    super(name);
+    this.name = ArgChecker.notEmpty(name, "name");
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public Class<Curve> getMarketDataType() {
+    return Curve.class;
+  }
+
+  @Override
+  public String getName() {
+    return name;
   }
 
 }
