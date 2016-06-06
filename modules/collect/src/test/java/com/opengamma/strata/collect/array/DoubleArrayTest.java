8ab0e2bad1e5e701cb1ca7be7ba4844ce73cc893
Stephen Colebourne <stephen@opengamma.com>
2016-06-06 16:21:53 +0100

    Allow concat to operate with varargs

--- modules/collect/src/test/java/com/opengamma/strata/collect/array/DoubleArrayTest.java
+++ modules/collect/src/test/java/com/opengamma/strata/collect/array/DoubleArrayTest.java
@@ -398,8 +398,9 @@ public class DoubleArrayTest {
   }
 
   //-------------------------------------------------------------------------
-  public void test_concat_array() {
+  public void test_concat_varargs() {
     DoubleArray test1 = DoubleArray.of(1d, 2d, 3d);
+    assertContent(test1.concat(0.5d, 0.6d, 0.7d), 1d, 2d, 3d, 0.5d, 0.6d, 0.7d);
     assertContent(test1.concat(new double[] {0.5d, 0.6d, 0.7d}), 1d, 2d, 3d, 0.5d, 0.6d, 0.7d);
     assertContent(test1.concat(EMPTY_DOUBLE_ARRAY), 1d, 2d, 3d);
     assertContent(DoubleArray.EMPTY.concat(new double[] {1d, 2d, 3d}), 1d, 2d, 3d);
