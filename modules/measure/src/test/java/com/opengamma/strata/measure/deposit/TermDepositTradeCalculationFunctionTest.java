0b6a0764fc35adfb97f74817c321e2e6a9a619fd
Stephen Colebourne <stephen@opengamma.com>
2016-06-15 16:20:49 +0100

    Term deposit measures

--- modules/measure/src/test/java/com/opengamma/strata/measure/deposit/TermDepositTradeCalculationFunctionTest.java
+++ modules/measure/src/test/java/com/opengamma/strata/measure/deposit/TermDepositTradeCalculationFunctionTest.java
@@ -7,7 +7,6 @@ package com.opengamma.strata.measure.deposit;
 
 import static com.opengamma.strata.basics.date.DayCounts.ACT_360;
 import static com.opengamma.strata.basics.date.DayCounts.THIRTY_360_ISDA;
-import static com.opengamma.strata.collect.TestHelper.coverPrivateConstructor;
 import static com.opengamma.strata.collect.TestHelper.date;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,10 +41,12 @@ import com.opengamma.strata.measure.Measures;
 import com.opengamma.strata.measure.curve.TestMarketDataMap;
 import com.opengamma.strata.measure.rate.RatesMarketDataLookup;
 import com.opengamma.strata.pricer.deposit.DiscountingTermDepositProductPricer;
+import com.opengamma.strata.pricer.deposit.DiscountingTermDepositTradePricer;
 import com.opengamma.strata.pricer.rate.RatesProvider;
 import com.opengamma.strata.product.TradeInfo;
 import com.opengamma.strata.product.common.BuySell;
 import com.opengamma.strata.product.deposit.ResolvedTermDeposit;
+import com.opengamma.strata.product.deposit.ResolvedTermDepositTrade;
 import com.opengamma.strata.product.deposit.TermDeposit;
 import com.opengamma.strata.product.deposit.TermDepositTrade;
 
@@ -55,6 +56,7 @@ import com.opengamma.strata.product.deposit.TermDepositTrade;
 @Test
 public class TermDepositTradeCalculationFunctionTest {
 
+  private static final ReferenceData REF_DATA = ReferenceData.standard();
   public static final TermDepositTrade TRADE = TermDepositTrade.builder()
       .info(TradeInfo.builder()
           .tradeDate(date(2015, 6, 1))
@@ -69,11 +71,11 @@ public class TermDepositTradeCalculationFunctionTest {
           .rate(0.002)
           .build())
       .build();
+  public static final ResolvedTermDepositTrade RTRADE = TRADE.resolve(REF_DATA);
 
-  private static final ReferenceData REF_DATA = ReferenceData.standard();
   private static final Currency CURRENCY = TRADE.getProduct().getCurrency();
   private static final CurveId DISCOUNT_CURVE_ID = CurveId.of("Default", "Discount");
-  private static final RatesMarketDataLookup RATES_LOOKUP = RatesMarketDataLookup.of(
+  public static final RatesMarketDataLookup RATES_LOOKUP = RatesMarketDataLookup.of(
       ImmutableMap.of(CURRENCY, DISCOUNT_CURVE_ID),
       ImmutableMap.of());
   private static final CalculationParameters PARAMS = CalculationParameters.of(RATES_LOOKUP);
@@ -94,17 +96,20 @@ public class TermDepositTradeCalculationFunctionTest {
     TermDepositTradeCalculationFunction function = new TermDepositTradeCalculationFunction();
     ScenarioMarketData md = marketData();
     RatesProvider provider = RATES_LOOKUP.ratesProvider(md.scenario(0));
-    DiscountingTermDepositProductPricer pricer = DiscountingTermDepositProductPricer.DEFAULT;
-    ResolvedTermDeposit resolved = TRADE.getProduct().resolve(REF_DATA);
-    CurrencyAmount expectedPv = pricer.presentValue(resolved, provider);
-    double expectedParRate = pricer.parRate(resolved, provider);
-    double expectedParSpread = pricer.parSpread(resolved, provider);
+    DiscountingTermDepositTradePricer pricer = DiscountingTermDepositTradePricer.DEFAULT;
+    CurrencyAmount expectedPv = pricer.presentValue(RTRADE, provider);
+    double expectedParRate = pricer.parRate(RTRADE, provider);
+    double expectedParSpread = pricer.parSpread(RTRADE, provider);
+    MultiCurrencyAmount expectedCurrencyExposure = pricer.currencyExposure(RTRADE, provider);
+    CurrencyAmount expectedCurrentCash = pricer.currentCash(RTRADE, provider);
 
     Set<Measure> measures = ImmutableSet.of(
         Measures.PRESENT_VALUE,
         Measures.PRESENT_VALUE_MULTI_CCY,
         Measures.PAR_RATE,
-        Measures.PAR_SPREAD);
+        Measures.PAR_SPREAD,
+        Measures.CURRENCY_EXPOSURE,
+        Measures.CURRENT_CASH);
     assertThat(function.calculate(TRADE, measures, PARAMS, md, REF_DATA))
         .containsEntry(
             Measures.PRESENT_VALUE, Result.success(CurrencyValuesArray.of(ImmutableList.of(expectedPv))))
@@ -113,7 +118,11 @@ public class TermDepositTradeCalculationFunctionTest {
         .containsEntry(
             Measures.PAR_RATE, Result.success(ValuesArray.of(ImmutableList.of(expectedParRate))))
         .containsEntry(
-            Measures.PAR_SPREAD, Result.success(ValuesArray.of(ImmutableList.of(expectedParSpread))));
+            Measures.PAR_SPREAD, Result.success(ValuesArray.of(ImmutableList.of(expectedParSpread))))
+        .containsEntry(
+            Measures.CURRENCY_EXPOSURE, Result.success(MultiCurrencyValuesArray.of(ImmutableList.of(expectedCurrencyExposure))))
+        .containsEntry(
+            Measures.CURRENT_CASH, Result.success(CurrencyValuesArray.of(ImmutableList.of(expectedCurrentCash))));
   }
 
   public void test_pv01() {
@@ -138,18 +147,13 @@ public class TermDepositTradeCalculationFunctionTest {
   }
 
   //-------------------------------------------------------------------------
-  private ScenarioMarketData marketData() {
+  static ScenarioMarketData marketData() {
     Curve curve = ConstantCurve.of(Curves.discountFactors("Test", ACT_360), 0.99);
     TestMarketDataMap md = new TestMarketDataMap(
         VAL_DATE,
         ImmutableMap.of(DISCOUNT_CURVE_ID, curve),
         ImmutableMap.of());
     return md;
-  }
-
-  //-------------------------------------------------------------------------
-  public void coverage() {
-    coverPrivateConstructor(TermDepositMeasureCalculations.class);
   }
 
 }
