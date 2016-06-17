d7c0faedafed27b6ef8e4933bc289dba7b0a46b2
Stephen Colebourne <stephen@opengamma.com>
2016-06-17 13:31:04 +0100

    Prepare for v0.15

--- modules/measure/src/main/java/com/opengamma/strata/measure/package-info.java
+++ modules/measure/src/main/java/com/opengamma/strata/measure/package-info.java
@@ -10,9 +10,9 @@
  * The measures can be accessed in three ways:
  * <ul>
  * <li>for one trade/position and one scenario, such as methods on
- *   {@code SwapMeasureCalculations}
+ *   {@code com.opengamma.strata.measure.swap.SwapTradeCalculations SwapTradeCalculations}
  * <li>for one trade/position and many scenarios, such as methods on
- *   {@code SwapMeasureCalculations}
+ *   {@code com.opengamma.strata.measure.swap.SwapTradeCalculations SwapTradeCalculations}
  * <li>for a mixed portfolio of trades/positions and many scenarios,
  *   see {@link com.opengamma.strata.calc.CalculationRunner CalculationRunner} and
  *   functions such as {@link com.opengamma.strata.measure.swap.SwapTradeCalculationFunction SwapCalculationFunction}
