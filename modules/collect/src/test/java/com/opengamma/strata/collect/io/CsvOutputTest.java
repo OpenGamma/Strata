6a34e592c4357a636d3ca94bb91d9b74dbb897d2
David Cervi <david@opengamma.com>
2016-12-30 15:36:01 +0000

    Added support for flexible line item separator in CsvOutput, Fixes #1439 (#1459)
    * Added support for flexible line item separator in CsvOutput, Fixes #1439
    
    * Added tab separator test

--- modules/collect/src/test/java/com/opengamma/strata/collect/io/CsvOutputTest.java
+++ modules/collect/src/test/java/com/opengamma/strata/collect/io/CsvOutputTest.java
@@ -19,40 +19,49 @@ import org.testng.annotations.Test;
 public class CsvOutputTest {
 
   private static final String LINE_SEP = System.lineSeparator();
+  private static final String LINE_ITEM_SEP_COMMA = ",";
+  private static final String LINE_ITEM_SEP_TAB = "\t";
+
 
   //-------------------------------------------------------------------------
   public void test_writeLines_alwaysQuote() {
     List<List<String>> rows = Arrays.asList(Arrays.asList("a", "x"), Arrays.asList("b", "y"));
     StringBuilder buf = new StringBuilder();
-    new CsvOutput(buf, "\n").writeLines(rows, true);
+    new CsvOutput(buf, "\n", LINE_ITEM_SEP_COMMA).writeLines(rows, true);
     assertEquals(buf.toString(), "\"a\",\"x\"\n\"b\",\"y\"\n");
   }
 
   public void test_writeLines_selectiveQuote_commaAndQuote() {
     List<List<String>> rows = Arrays.asList(Arrays.asList("a", "1,000"), Arrays.asList("b\"c", "y"));
     StringBuilder buf = new StringBuilder();
-    new CsvOutput(buf, "\n").writeLines(rows, false);
+    new CsvOutput(buf, "\n", LINE_ITEM_SEP_COMMA).writeLines(rows, false);
     assertEquals(buf.toString(), "a,\"1,000\"\n\"b\"\"c\",y\n");
   }
 
   public void test_writeLines_selectiveQuote_trimmable() {
     List<List<String>> rows = Arrays.asList(Arrays.asList("a", " x"), Arrays.asList("b ", "y"));
     StringBuilder buf = new StringBuilder();
-    new CsvOutput(buf, "\n").writeLines(rows, false);
+    new CsvOutput(buf, "\n", LINE_ITEM_SEP_COMMA).writeLines(rows, false);
     assertEquals(buf.toString(), "a,\" x\"\n\"b \",y\n");
   }
 
   public void test_writeLines_systemNewLine() {
     List<List<String>> rows = Arrays.asList(Arrays.asList("a", "x"), Arrays.asList("b", "y"));
     StringBuilder buf = new StringBuilder();
-    new CsvOutput(buf).writeLines(rows, false);
+    new CsvOutput(buf, LINE_ITEM_SEP_COMMA).writeLines(rows, false);
     assertEquals(buf.toString(), "a,x" + LINE_SEP + "b,y" + LINE_SEP);
   }
 
   public void test_writeLine_selectiveQuote() {
     StringBuilder buf = new StringBuilder();
-    new CsvOutput(buf, "\n").writeLine(Arrays.asList("a", "1,000"));
+    new CsvOutput(buf, "\n", LINE_ITEM_SEP_COMMA).writeLine(Arrays.asList("a", "1,000"));
     assertEquals(buf.toString(), "a,\"1,000\"\n");
+  }
+
+  public void test_writeLines_tab_separated() {
+    StringBuilder buf = new StringBuilder();
+    new CsvOutput(buf, "\n", LINE_ITEM_SEP_TAB).writeLine(Arrays.asList("a", "1,000"));
+    assertEquals(buf.toString(), "a\t\"1,000\"\n");
   }
 
 }
