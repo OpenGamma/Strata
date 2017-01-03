8dab5411e9bdc2e9a71530f719dd65a2f27ed196
Stephen Colebourne <scolebourne@joda.org>
2017-01-03 10:24:01 +0000

    Add list bean for CalculationTarget (#1457)
    Allows a list of targets to be serialized

---
+++ modules/basics/src/test/java/com/opengamma/strata/basics/CalculationTargetListTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2016 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.basics;
+
+import static com.opengamma.strata.collect.TestHelper.assertSerialization;
+import static com.opengamma.strata.collect.TestHelper.coverImmutableBean;
+import static org.testng.Assert.assertEquals;
+
+import java.io.Serializable;
+
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Test {@link CalculationTargetList}.
+ */
+@Test
+public class CalculationTargetListTest {
+
+  private static final CalculationTarget TARGET1 = new TestTarget(1);
+  private static final CalculationTarget TARGET2 = new TestTarget(2);
+
+  //-------------------------------------------------------------------------
+  public void test_array0() {
+    CalculationTargetList test = CalculationTargetList.of();
+    assertEquals(test.getTargets(), ImmutableList.of());
+  }
+
+  public void test_array2() {
+    CalculationTargetList test = CalculationTargetList.of(TARGET1, TARGET2);
+    assertEquals(test.getTargets(), ImmutableList.of(TARGET1, TARGET2));
+  }
+
+  public void test_collection1() {
+    CalculationTargetList test = CalculationTargetList.of(ImmutableList.of(TARGET1));
+    assertEquals(test.getTargets(), ImmutableList.of(TARGET1));
+  }
+
+  //-------------------------------------------------------------------------
+  public void coverage() {
+    CalculationTargetList test = CalculationTargetList.of(TARGET1, TARGET2);
+    coverImmutableBean(test);
+  }
+
+  public void test_serialization() {
+    CalculationTargetList test = CalculationTargetList.of(TARGET1, TARGET2);
+    assertSerialization(test);
+  }
+
+  //-------------------------------------------------------------------------
+  static class TestTarget implements CalculationTarget, Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int value;
+
+    public TestTarget(int value) {
+      this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      return obj instanceof TestTarget && value == ((TestTarget) obj).value;
+    }
+
+    @Override
+    public int hashCode() {
+      return value;
+    }
+  }
+
+}
