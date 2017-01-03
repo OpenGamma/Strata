15c8bea42ce10e2f01f033fcf329556e97d4ac05
Stephen Colebourne <stephen@opengamma.com>
2017-01-03 10:07:18 +0000

    Add list bean for CalculationTarget
    Allows a list of targets to be serialized

--- modules/basics/src/main/java/com/opengamma/strata/basics/CalculationTarget.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/CalculationTarget.java
@@ -8,7 +8,7 @@ package com.opengamma.strata.basics;
 /**
  * The target of calculation within a system.
  * <p>
- * All financial instruments that can be the target of calculations implement this market interface.
+ * All financial instruments that can be the target of calculations implement this marker interface.
  * For example, a trade or position.
  * <p>
  * All implementations of this interface must be immutable and thread-safe.
