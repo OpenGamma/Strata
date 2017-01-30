fdb424066e40134870f65b2389ed35c5aa764ecc
marc-henrard <marc@opengamma.com>
2017-01-30 16:11:19 +0000

    Remove ArgChecker in OvernightRate which was counterproductive for +1 publication lag

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/rate/DiscountOvernightIndexRates.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/rate/DiscountOvernightIndexRates.java
@@ -207,7 +207,6 @@ public final class DiscountOvernightIndexRates
   public double periodRate(OvernightIndexObservation startDateObservation, LocalDate endDate) {
     LocalDate effectiveDate = startDateObservation.getEffectiveDate();
     ArgChecker.inOrderNotEqual(effectiveDate, endDate, "startDate", "endDate");
-    ArgChecker.inOrderOrEqual(getValuationDate(), effectiveDate, "valuationDate", "startDate");
     double accrualFactor = startDateObservation.getIndex().getDayCount().yearFraction(effectiveDate, endDate);
     return simplyCompoundForwardRate(effectiveDate, endDate, accrualFactor);
   }
