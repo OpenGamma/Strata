504b4aabc1c7bce9ad0f25052483c1d5f4da55b6
Stephen Colebourne <scolebourne@joda.org>
2017-06-28 10:53:21 +0100

    Extend de-adjustments (#1526)
    * Extend de-adjustments
    
    Start date:
    Bring checks more in line with other dates
    
    End date:
    De-adjust using the roll convention as per other dates
    
    * Revert part of the de-adjustment

--- modules/basics/src/test/java/com/opengamma/strata/basics/schedule/PeriodicScheduleTest.java
+++ modules/basics/src/test/java/com/opengamma/strata/basics/schedule/PeriodicScheduleTest.java
@@ -402,6 +402,14 @@ public class PeriodicScheduleTest {
         {APR_01, OCT_17, P1M, null, DAY_17, MAY_19, AUG_18, BDA_NONE,
             ImmutableList.of(APR_01, MAY_17, JUN_17, JUL_17, AUG_17, OCT_17),
             ImmutableList.of(APR_01, MAY_19, JUN_17, JUL_17, AUG_18, OCT_17), DAY_17},
+        // pre-adjusted end date, change needed
+        {JUL_17, AUG_18, P1M, null, DAY_17, null, null, BDA_NONE,
+            ImmutableList.of(JUL_17, AUG_17),
+            ImmutableList.of(JUL_17, AUG_18), DAY_17},
+        // pre-adjusted end date, change needed, with adjustment
+        {JUL_17, AUG_18, P1M, null, DAY_17, null, null, BDA,
+            ImmutableList.of(JUL_17, AUG_17),
+            ImmutableList.of(JUL_17, AUG_18), DAY_17},
 
         // TERM period
         {JUN_04, SEP_17, TERM, STUB_NONE, null, null, null, null,
