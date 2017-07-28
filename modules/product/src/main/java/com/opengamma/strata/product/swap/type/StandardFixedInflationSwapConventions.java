a9f7920b2da57752537f92c9aa21770baa91acbc
Brian <brian@opengamma.com>
2017-07-28 11:59:37 +0100

    WIP

--- modules/product/src/main/java/com/opengamma/strata/product/swap/type/StandardFixedInflationSwapConventions.java
+++ modules/product/src/main/java/com/opengamma/strata/product/swap/type/StandardFixedInflationSwapConventions.java
@@ -194,7 +194,6 @@ final class StandardFixedInflationSwapConventions {
   }
 
   //-------------------------------------------------------------------------
-
   /**
    * Restricted constructor.
    */
