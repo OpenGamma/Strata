b1388d13d81d80820870854451a4732bf1a9960d
Andrei Ruse <andrei@opengamma.com>
2017-08-03 08:28:09 +0100

    Addressing PR feedback

--- modules/collect/src/main/java/com/opengamma/strata/collect/io/CharSources.java
+++ modules/collect/src/main/java/com/opengamma/strata/collect/io/CharSources.java
@@ -21,12 +21,13 @@ import com.google.common.io.Resources;
  */
 public final class CharSources {
 
-  private CharSources() { }
+  private CharSources() {
+  }
 
   /**
    * Obtains an instance of {@link CharSource} from a file name, specified as a String.
    *
-   * @param  fileName the file name, as a String
+   * @param fileName  the file name, as a String
    * @return  a new instance of {@link CharSource} with UTF-8 for charset.
    */
   public static CharSource ofFileName(String fileName) {
@@ -37,8 +38,8 @@ public final class CharSources {
    * Obtains an instance of {@link CharSource} from a file name, specified as a String.
    * This also takes in a specific character set, as a {@link Charset}.
    *
-   * @param  fileName the file name, as a String
-   * @param  charset the charset to build the new CharSource based on
+   * @param fileName  the file name, as a String
+   * @param charset  the charset to build the new CharSource based on
    * @return  a new instance of {@link CharSource}
    */
   public static CharSource ofFileName(String fileName, Charset charset) {
@@ -49,7 +50,7 @@ public final class CharSources {
   /**
    * Obtains an instance of {@link CharSource} from a file object, specified as a {@link File}.
    *
-   * @param  file the file object
+   * @param file  the file object
    * @return  a new instance of {@link CharSource} with UTF-8 for charset.
    */
   public static CharSource ofFile(File file) {
@@ -60,8 +61,8 @@ public final class CharSources {
    * Obtains an instance of {@link CharSource} from a file object, specified as a {@link File}.
    * This also takes in a specific character set, as a {@link Charset}.
    *
-   * @param  file the file object
-   * @param  charset the charset to build the new CharSource based on
+   * @param file  the file object
+   * @param charset  the charset to build the new CharSource based on
    * @return  a new instance of {@link CharSource}
    */
   public static CharSource ofFile(File file, Charset charset) {
@@ -72,7 +73,7 @@ public final class CharSources {
   /**
    * Obtains an instance of {@link CharSource} from a file path, specified as a {@link Path}.
    *
-   * @param  path the path to create a {@link CharSource} from
+   * @param path  the path to create a {@link CharSource} from
    * @return  a new instance of {@link CharSource} with UTF-8 for charset.
    */
   public static CharSource ofPath(Path path) {
@@ -83,20 +84,19 @@ public final class CharSources {
    * Obtains an instance of {@link CharSource} from a file path, specified as a {@link Path}.
    * This also takes in a specific character set, as a {@link Charset}.
    *
-   * @param  path the path to create a {@link CharSource} from
-   * @param  charset the charset to build the new CharSource based on
+   * @param path  the path to create a {@link CharSource} from
+   * @param charset  the charset to build the new CharSource based on
    * @return  a new instance of {@link CharSource}
    */
   public static CharSource ofPath(Path path, Charset charset) {
     return MoreFiles.asCharSource(path, charset);
   }
-
-
+  
   //---------------------------------------------------------------------------------------------
   /**
    * Obtains an instance of {@link CharSource} from a URL, specified as a {@link URL} object.
    *
-   * @param  url the url to create a {@link CharSource} from
+   * @param url  the url to create a {@link CharSource} from
    * @return  a new instance of {@link CharSource} with UTF-8 for charset.
    */
   public static CharSource ofURL(URL url) {
