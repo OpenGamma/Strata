c99bf566848311f10f27d5e48f7e47f4031fa3c2
Andrei Ruse <andreiruse@users.noreply.github.com>
2017-08-03 09:27:54 +0100

    Adding CharSources + unit test + test resource file (#1538)
    * Adding CharSources + unit test + test resource file
    
    * Adding copyright header
    
    * Removing default charset usage, replacing it with a specific charset (UTF8)
    
    * Replacing default charset in String.getBytes()
    
    * Addressing PR feedback - moving classes, and adding/removing some factory methods
    
    * Fixing use of illegal API
    
    * Addressing PR feedback

---
+++ modules/collect/src/main/java/com/opengamma/strata/collect/io/CharSources.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2017 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.collect.io;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharSource;
+import com.google.common.io.Files;
+import com.google.common.io.MoreFiles;
+import com.google.common.io.Resources;
+
+/**
+ * Helper that allows {@code CharSource} objects to be created.
+ */
+public final class CharSources {
+
+  private CharSources() {
+  }
+
+  /**
+   * Obtains an instance of {@link CharSource} from a file name, specified as a String.
+   *
+   * @param fileName  the file name, as a String
+   * @return  a new instance of {@link CharSource} with UTF-8 for charset.
+   */
+  public static CharSource ofFileName(String fileName) {
+    return Files.asCharSource(new File(fileName), Charsets.UTF_8);
+  }
+
+  /**
+   * Obtains an instance of {@link CharSource} from a file name, specified as a String.
+   * This also takes in a specific character set, as a {@link Charset}.
+   *
+   * @param fileName  the file name, as a String
+   * @param charset  the charset to build the new CharSource based on
+   * @return  a new instance of {@link CharSource}
+   */
+  public static CharSource ofFileName(String fileName, Charset charset) {
+    return Files.asCharSource(new File(fileName), charset);
+  }
+
+  //---------------------------------------------------------------------------------------------
+  /**
+   * Obtains an instance of {@link CharSource} from a file object, specified as a {@link File}.
+   *
+   * @param file  the file object
+   * @return  a new instance of {@link CharSource} with UTF-8 for charset.
+   */
+  public static CharSource ofFile(File file) {
+    return Files.asCharSource(file, Charsets.UTF_8);
+  }
+
+  /**
+   * Obtains an instance of {@link CharSource} from a file object, specified as a {@link File}.
+   * This also takes in a specific character set, as a {@link Charset}.
+   *
+   * @param file  the file object
+   * @param charset  the charset to build the new CharSource based on
+   * @return  a new instance of {@link CharSource}
+   */
+  public static CharSource ofFile(File file, Charset charset) {
+    return Files.asCharSource(file, charset);
+  }
+
+  //---------------------------------------------------------------------------------------------
+  /**
+   * Obtains an instance of {@link CharSource} from a file path, specified as a {@link Path}.
+   *
+   * @param path  the path to create a {@link CharSource} from
+   * @return  a new instance of {@link CharSource} with UTF-8 for charset.
+   */
+  public static CharSource ofPath(Path path) {
+    return MoreFiles.asCharSource(path, Charsets.UTF_8);
+  }
+
+  /**
+   * Obtains an instance of {@link CharSource} from a file path, specified as a {@link Path}.
+   * This also takes in a specific character set, as a {@link Charset}.
+   *
+   * @param path  the path to create a {@link CharSource} from
+   * @param charset  the charset to build the new CharSource based on
+   * @return  a new instance of {@link CharSource}
+   */
+  public static CharSource ofPath(Path path, Charset charset) {
+    return MoreFiles.asCharSource(path, charset);
+  }
+  
+  //---------------------------------------------------------------------------------------------
+  /**
+   * Obtains an instance of {@link CharSource} from a URL, specified as a {@link URL} object.
+   *
+   * @param url  the url to create a {@link CharSource} from
+   * @return  a new instance of {@link CharSource} with UTF-8 for charset.
+   */
+  public static CharSource ofURL(URL url) {
+    return Resources.asCharSource(url, Charsets.UTF_8);
+  }
+
+  /**
+   * Obtains an instance of {@link CharSource} from an URL, specified as a {@link URL} object.
+   * This also takes in a specific character set, as a {@link Charset}.
+   *
+   * @param url  the url to create a {@link CharSource} from
+   * @param charset  the charset to build the new CharSource based on
+   * @return  a new instance of {@link CharSource}.
+   */
+  public static CharSource ofURL(URL url, Charset charset) {
+    return Resources.asCharSource(url, charset);
+  }
+
+  //---------------------------------------------------------------------------------------------
+  /**
+   * Obtains an instance of {@link CharSource} from a text variable, specified as a {@link String} object.
+   *
+   * @param content  the text to create a {@link CharSource} for
+   * @return  a new instance of {@link CharSource} with UTF-8 for charset
+   */
+  public static CharSource ofContent(String content) {
+    return CharSource.wrap(content);
+  }
+
+  //---------------------------------------------------------------------------------------------
+  /**
+   * Obtains an instance of {@link CharSource} from a text variable, specified as a byte array.
+   * 
+   * @param content  the text to create a {@link CharSource} for
+   * @return  a new instance of {@link CharSource} with UTF-8 for charset
+   */
+  public static CharSource ofContent(byte[] content) {
+    return CharSource.wrap(new String(content, Charsets.UTF_8));
+  }
+
+  /**
+   * Obtains an instance of {@link CharSource} from a text variable, specified as a byte array.
+   * This also takes in a specific character set, as a {@link Charset}.
+   * 
+   * @param content  the text to create a {@link CharSource} for
+   * @param charset  the charset to build the new CharSource based on
+   * @return  a new instance of {@link CharSource}
+   */
+  public static CharSource ofContent(byte[] content, Charset charset) {
+    return CharSource.wrap(new String(content, charset));
+  }
+}
---
+++ modules/collect/src/test/java/com/opengamma/strata/collect/io/CharSourcesTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2017 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.collect.io;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.testng.annotations.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharSource;
+import com.opengamma.strata.collect.TestHelper;
+
+/**
+ * Tests {@link CharSources}
+ */
+@Test
+public class CharSourcesTest {
+
+  private final String fileName = "src/test/resources/com/opengamma/strata/collect/io/utf16le.txt";
+
+  public void testPrivateConstructor() throws Exception {
+    TestHelper.coverPrivateConstructor(CharSources.class);
+  }
+
+  public void testOfFileName() throws Exception {
+    CharSource charSource = CharSources.ofFileName(fileName);
+    assertEquals(charSource.readFirstLine(), "H\u0000e\u0000l\u0000l\u0000o\u0000");
+    assertEquals(charSource.length(), 10);
+  }
+
+  public void testOfFileNameWithCharset() throws Exception {
+    CharSource charSource = CharSources.ofFileName(fileName, Charsets.UTF_16LE);
+    assertEquals(charSource.readFirstLine(), "Hello");
+    assertEquals(charSource.length(), 5);
+  }
+
+  public void testOfFile() throws Exception {
+    CharSource charSource = CharSources.ofFile(new File(fileName));
+    assertEquals(charSource.readFirstLine(), "H\u0000e\u0000l\u0000l\u0000o\u0000");
+    assertEquals(charSource.length(), 10);
+  }
+
+  public void testOfFileWithCharset() throws Exception {
+    CharSource charSource = CharSources.ofFile(new File(fileName), Charsets.UTF_16LE);
+    assertEquals(charSource.readFirstLine(), "Hello");
+    assertEquals(charSource.length(), 5);
+  }
+
+  public void testOfPath() throws Exception {
+    CharSource charSource = CharSources.ofPath(Paths.get(fileName));
+    assertEquals(charSource.readFirstLine(), "H\u0000e\u0000l\u0000l\u0000o\u0000");
+    assertEquals(charSource.length(), 10);
+  }
+
+  public void testOfPathWithCharset() throws Exception {
+    CharSource charSource = CharSources.ofPath(Paths.get(fileName), Charsets.UTF_16LE);
+    assertEquals(charSource.readFirstLine(), "Hello");
+    assertEquals(charSource.length(), 5);
+  }
+
+  @Test
+  public void testOfURL() throws Exception {
+    String fullPathToFile = "file:///" + System.getProperty("user.dir") + "/" + fileName;
+    URL url = new URL(fullPathToFile);
+    CharSource charSource = CharSources.ofURL(url);
+    assertEquals(charSource.readFirstLine(), "H\u0000e\u0000l\u0000l\u0000o\u0000");
+  }
+
+  @Test
+  public void testOfURLWithCharset() throws Exception {
+    String fullPathToFile = "file:///" + System.getProperty("user.dir") + "/" + fileName;
+    URL url = new URL(fullPathToFile);
+    CharSource charSource = CharSources.ofURL(url, Charsets.UTF_16LE);
+    assertEquals(charSource.readFirstLine(), "Hello");
+  }
+
+  @Test
+  public void testOfContentString() throws Exception {
+    CharSource charSource = CharSources.ofContent("H\u0000e\u0000l\u0000l\u0000o\u0000");
+    assertEquals(charSource.readFirstLine(), "H\u0000e\u0000l\u0000l\u0000o\u0000");
+    assertEquals(charSource.length(), 10);
+  }
+
+  @Test
+  public void testOfContentByteArray() throws Exception {
+    byte[] inputText = "H\u0000e\u0000l\u0000l\u0000o\u0000".getBytes(Charsets.UTF_8);
+    CharSource charSource = CharSources.ofContent(inputText);
+    assertEquals(charSource.readFirstLine(), "H\u0000e\u0000l\u0000l\u0000o\u0000");
+    assertEquals(charSource.length(), 10);
+  }
+
+  @Test
+  public void testOfContentByteArrayWithCharset() throws Exception {
+    byte[] inputText = "H\u0000e\u0000l\u0000l\u0000o\u0000".getBytes(Charsets.UTF_8);
+    CharSource charSource = CharSources.ofContent(inputText, Charsets.UTF_16LE);
+    assertEquals(charSource.readFirstLine(), "Hello");
+    assertEquals(charSource.length(), 5);
+  }
+}
