1f5c21d989cbcaece4bdbdc7df44a657f6d10e36
Yuki Iwashita <yukiiwashita@users.noreply.github.com>
2017-10-30 15:34:16 +0000

    date logic for fx vol node (#1591)

--- modules/measure/src/test/java/com/opengamma/strata/measure/fxopt/BlackFxOptionInterpolatedNodalSurfaceVolatilitiesSpecificationTest.java
+++ modules/measure/src/test/java/com/opengamma/strata/measure/fxopt/BlackFxOptionInterpolatedNodalSurfaceVolatilitiesSpecificationTest.java
@@ -119,16 +119,17 @@ public class BlackFxOptionInterpolatedNodalSurfaceVolatilitiesSpecificationTest 
             .timeInterpolator(PCHIP)
             .strikeInterpolator(DOUBLE_QUADRATIC)
             .build();
-    ZonedDateTime dateTime = LocalDate.of(2017, 9, 25).atStartOfDay().atZone(ZoneId.of("Europe/London"));
+    LocalDate date = LocalDate.of(2017, 9, 25);
+    ZonedDateTime dateTime = date.atStartOfDay().atZone(ZoneId.of("Europe/London"));
     DoubleArray parameters = DoubleArray.of(0.19, 0.15, 0.13, 0.14, 0.14, 0.11, 0.09, 0.09, 0.11, 0.09, 0.07, 0.07);
     BlackFxOptionSurfaceVolatilities computed = base.volatilities(dateTime, parameters, REF_DATA);
+    DaysAdjustment expOffset = DaysAdjustment.ofBusinessDays(-2, NY_LO);
     double[] expiries = new double[STRIKES.size() * TENORS.size()];
     double[] strikes = new double[STRIKES.size() * TENORS.size()];
     ImmutableList.Builder<ParameterMetadata> paramMetadata = ImmutableList.builder();
     for (int i = 0; i < TENORS.size(); ++i) {
       double expiry = ACT_365F.relativeYearFraction(
-          dateTime.toLocalDate(),
-          BDA.adjust(SPOT_OFFSET.adjust(dateTime.toLocalDate(), REF_DATA).plus(TENORS.get(i)), REF_DATA));
+          date, expOffset.adjust(BDA.adjust(SPOT_OFFSET.adjust(date, REF_DATA).plus(TENORS.get(i)), REF_DATA), REF_DATA));
       for (int j = 0; j < STRIKES.size(); ++j) {
         paramMetadata.add(FxVolatilitySurfaceYearFractionParameterMetadata.of(expiry, SimpleStrike.of(STRIKES.get(j)), GBP_USD));
         expiries[STRIKES.size() * i + j] = expiry;
