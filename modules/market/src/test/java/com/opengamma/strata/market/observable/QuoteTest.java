a3b854d37571f9b11cd27eb846b6adde24622cf4
Andrei Ruse <andrei@opengamma.com>
2017-11-20 12:04:46 +0000

    Adding Quote object and unit test

---
+++ modules/market/src/test/java/com/opengamma/strata/market/observable/QuoteTest.java
@@ -0,0 +1,37 @@
+package com.opengamma.strata.market.observable;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.strata.basics.StandardId;
+
+@Test
+public class QuoteTest {
+  private static final QuoteId quoteId1 = QuoteId.of(StandardId.of("og", "id1"));
+
+  public void test_of_QuoteId() throws Exception {
+    Quote test = Quote.of(quoteId1, 1.234);
+    assertEquals(test.getQuoteId(), quoteId1);
+    assertEquals(test.getValue(), 1.234);
+    assertEquals(test.toString(), "Quote{quoteId=QuoteId{standardId=og~id1, fieldName=MarketValue, observableSource=None}, value=1.234}");
+  }
+
+
+  public void test_of_Strings() throws Exception {
+    Quote test = Quote.of("og", "id1", 1.2345);
+    assertEquals(test.getQuoteId(), quoteId1);
+    assertEquals(test.getValue(), 1.2345);
+    assertEquals(test.toString(), "Quote{quoteId=QuoteId{standardId=og~id1, fieldName=MarketValue, observableSource=None}, value=1.2345}");
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Argument 'scheme' with value '' must match pattern:.+")
+  public void test_of_EmptySchemeId() throws Exception {
+    Quote test = Quote.of("", "notEmpty", 1.2345);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Argument 'value' with value '' must match pattern:.+")
+  public void test_of_EmptyId() throws Exception {
+    Quote test = Quote.of("notEmpty", "", 1.2345);
+  }
+}
