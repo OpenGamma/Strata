e98f3ae423aab5ce20100e2cc510313afb8813d7
Stephen Colebourne <stephen@opengamma.com>
2017-11-21 12:08:11 +0000

    Additional tidyup

--- modules/market/src/test/java/com/opengamma/strata/market/observable/QuoteTest.java
+++ modules/market/src/test/java/com/opengamma/strata/market/observable/QuoteTest.java
@@ -6,12 +6,17 @@
 package com.opengamma.strata.market.observable;
 
 import static com.opengamma.strata.collect.TestHelper.assertThrowsIllegalArg;
+import static com.opengamma.strata.collect.TestHelper.coverBeanEquals;
+import static com.opengamma.strata.collect.TestHelper.coverImmutableBean;
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
 import com.opengamma.strata.basics.StandardId;
 
+/**
+ * Test {@link Quote}.
+ */
 @Test
 public class QuoteTest {
   private static final QuoteId QUOTE_ID_1 = QuoteId.of(StandardId.of("og", "id1"));
@@ -20,19 +25,17 @@ public class QuoteTest {
     Quote test = Quote.of(QUOTE_ID_1, 1.234);
     assertEquals(test.getQuoteId(), QUOTE_ID_1);
     assertEquals(test.getValue(), 1.234);
-    assertEquals(test.toString(), "Quote{quoteId=QuoteId{standardId=og~id1, fieldName=MarketValue, observableSource=None}, value=1.234}");
   }
 
-
-  public void test_of_Strings() throws Exception {
-    Quote test = Quote.of("og", "id1", 1.2345);
-    assertEquals(test.getQuoteId(), QUOTE_ID_1);
-    assertEquals(test.getValue(), 1.2345);
-    assertEquals(test.toString(), "Quote{quoteId=QuoteId{standardId=og~id1, fieldName=MarketValue, observableSource=None}, value=1.2345}");
+  public void test_of_nullQuoteId() throws Exception {
+    assertThrowsIllegalArg(() -> Quote.of(null, 1.2345));
   }
 
-  public void test_of_EmptySchemeId() throws Exception {
-    assertThrowsIllegalArg(() -> Quote.of("", "notEmpty", 1.2345), "Argument 'scheme' with value '' must match pattern:.+");
-    assertThrowsIllegalArg(() -> Quote.of("notEmpty", "", 1.2345), "Argument 'value' with value '' must match pattern:.+");
+  public void coverage() {
+    Quote test = Quote.of(QUOTE_ID_1, 1.234);
+    coverImmutableBean(test);
+    Quote test2 = Quote.of(QuoteId.of(StandardId.of("a", "b")), 4.321);
+    coverBeanEquals(test, test2);
   }
+
 }
