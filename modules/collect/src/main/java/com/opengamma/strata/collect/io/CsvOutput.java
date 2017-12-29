85c2d77b7851ab7014184e4e2cde71641cd57c72
Stephen Colebourne <scolebourne@joda.org>
2017-12-29 11:25:22 +0000

    CSV output standard and safe modes (#1633)
    Allow callers to choose whether they want the extra-safe encoding

--- modules/collect/src/main/java/com/opengamma/strata/collect/io/CsvOutput.java
+++ modules/collect/src/main/java/com/opengamma/strata/collect/io/CsvOutput.java
@@ -23,8 +23,12 @@ import com.opengamma.strata.collect.Unchecked;
  * Each line in the CSV file will consist of comma separated entries.
  * Each entry may be quoted using a double quote.
  * If an entry contains a double quote, comma or trimmable whitespace, it will be quoted.
+ * If an entry starts with '=' or '@', it will be quoted.
  * Two double quotes will be used to escape a double quote.
- * If it starts with an expression character, the quote section will be preceeded by equals.
+ * <p>
+ * There are two modes of output.
+ * Standard mode provides the encoding described above which is accepted by most CSV parsers.
+ * Safe mode provides extra encoding to protect unsafe content from being run as a script in tools like Excel.
  */
 public final class CsvOutput {
 
@@ -34,6 +38,8 @@ public final class CsvOutput {
   private static final Pattern FP_REGEX = Pattern.compile(
           "(" + DIGITS + "(\\.)?(" + DIGITS + "?)(" + EXPONENT + ")?)|" +
           "(\\.(" + DIGITS + ")(" + EXPONENT + ")?)");
+  private static final String COMMA = ",";
+  private static final String NEW_LINE = System.lineSeparator();
 
   /**
    * The header row, ordered as the headers appear in the file.
@@ -44,26 +50,132 @@ public final class CsvOutput {
    */
   private final String newLine;
   /**
-   * The line item separator
+   * The line item separator.
    */
   private final String separator;
+  /**
+   * Whether expressions should be safely encoded
+   */
+  private final boolean safeExpressions;
+
+  //-------------------------------------------------------------------------
+  /**
+   * Creates an instance, using the system default line separator and using a comma separator.
+   * <p>
+   * See the standard quoting rules in the class-level documentation.
+   * 
+   * @param underlying  the destination to write to
+   * @return the CSV outputter
+   */
+  public static CsvOutput standard(Appendable underlying) {
+    return new CsvOutput(underlying, NEW_LINE, COMMA, false);
+  }
+
+  /**
+   * Creates an instance, allowing the new line character to be controlled and using a comma separator.
+   * <p>
+   * See the standard quoting rules in the class-level documentation.
+   * 
+   * @param underlying  the destination to write to
+   * @param newLine  the new line string
+   * @return the CSV outputter
+   */
+  public static CsvOutput standard(Appendable underlying, String newLine) {
+    return new CsvOutput(underlying, newLine, COMMA, false);
+  }
+
+  /**
+   * Creates an instance, allowing the new line character to be controlled, specifying the separator.
+   * <p>
+   * See the standard quoting rules in the class-level documentation.
+   * 
+   * @param underlying  the destination to write to
+   * @param newLine  the new line string
+   * @param separator  the separator used to separate each field, typically a comma, but a tab is sometimes used
+   * @return the CSV outputter
+   */
+  public static CsvOutput standard(Appendable underlying, String newLine, String separator) {
+    return new CsvOutput(underlying, newLine, separator, false);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Creates an instance, using the system default line separator and using a comma separator.
+   * <p>
+   * This applies the standard quoting rules from the class-level documentation, plus an additional rule.
+   * If an entry starts with an expression character, '=', '@', '+' or '-', the entry
+   * will be quoted and the quote section will be preceeded by equals.
+   * Thus, the string '=Foo' will be written as '="=Foo"'.
+   * This avoids the string being treated as an expression by tools like Excel.
+   * Simple numbers are not quoted.
+   * Thus, the number '-1234' will still be written as '-1234'.
+   * 
+   * @param underlying  the destination to write to
+   * @return the CSV outputter
+   */
+  public static CsvOutput safe(Appendable underlying) {
+    return new CsvOutput(underlying, NEW_LINE, COMMA, true);
+  }
+
+  /**
+   * Creates an instance, allowing the new line character to be controlled and using a comma separator.
+   * <p>
+   * This applies the standard quoting rules from the class-level documentation, plus an additional rule.
+   * If an entry starts with an expression character, '=', '@', '+' or '-', the entry
+   * will be quoted and the quote section will be preceeded by equals.
+   * Thus, the string '=Foo' will be written as '="=Foo"'.
+   * This avoids the string being treated as an expression by tools like Excel.
+   * Simple numbers are not quoted.
+   * Thus, the number '-1234' will still be written as '-1234'.
+   * 
+   * @param underlying  the destination to write to
+   * @param newLine  the new line string
+   * @return the CSV outputter
+   */
+  public static CsvOutput safe(Appendable underlying, String newLine) {
+    return new CsvOutput(underlying, newLine, COMMA, true);
+  }
+
+  /**
+   * Creates an instance, allowing the new line character to be controlled, specifying the separator.
+   * <p>
+   * This applies the standard quoting rules from the class-level documentation, plus an additional rule.
+   * If an entry starts with an expression character, '=', '@', '+' or '-', the entry
+   * will be quoted and the quote section will be preceeded by equals.
+   * Thus, the string '=Foo' will be written as '="=Foo"'.
+   * This avoids the string being treated as an expression by tools like Excel.
+   * Simple numbers are not quoted.
+   * Thus, the number '-1234' will still be written as '-1234'.
+   * 
+   * @param underlying  the destination to write to
+   * @param newLine  the new line string
+   * @param separator  the separator used to separate each field, typically a comma, but a tab is sometimes used
+   * @return the CSV outputter
+   */
+  public static CsvOutput safe(Appendable underlying, String newLine, String separator) {
+    return new CsvOutput(underlying, newLine, separator, true);
+  }
 
   //-------------------------------------------------------------------------
   /**
    * Creates an instance, using the system default line separator and using a comma separator.
    * 
-   * @param underlying  the underlying writer
+   * @param underlying  the destination to write to
+   * @deprecated Use one of the static factory methods
    */
+  @Deprecated
   public CsvOutput(Appendable underlying) {
-    this(underlying, System.lineSeparator(), ",");
+    this(underlying, NEW_LINE, ",");
   }
 
   /**
    * Creates an instance, allowing the new line character to be controlled and using a comma separator.
    * 
-   * @param underlying  the underlying writer
+   * @param underlying  the destination to write to
    * @param newLine  the new line string
+   * @deprecated Use one of the static factory methods
    */
+  @Deprecated
   public CsvOutput(Appendable underlying, String newLine) {
     this(underlying, newLine, ",");
   }
@@ -71,14 +183,22 @@ public final class CsvOutput {
   /**
    * Creates an instance, allowing the new line character to be controlled, specifying the separator.
    * 
-   * @param underlying  the underlying writer
+   * @param underlying  the destination to write to
    * @param newLine  the new line string
    * @param separator  the separator used to separate each field, typically a comma, but a tab is sometimes used
+   * @deprecated Use one of the static factory methods
    */
+  @Deprecated
   public CsvOutput(Appendable underlying, String newLine, String separator) {
+    this(underlying, newLine, separator, true);
+  }
+
+  // creates an instance
+  private CsvOutput(Appendable underlying, String newLine, String separator, boolean safeExpressions) {
     this.underlying = ArgChecker.notNull(underlying, "underlying");
     this.newLine = newLine;
     this.separator = separator;
+    this.safeExpressions = safeExpressions;
   }
 
   //------------------------------------------------------------------------
@@ -149,6 +269,7 @@ public final class CsvOutput {
         isExpressionPrefix(entry);
   }
 
+  // checks if quoting should be applied
   private boolean isExpressionPrefix(String entry) {
     if (entry.isEmpty()) {
       return false;
@@ -157,7 +278,7 @@ public final class CsvOutput {
     if (first == '=' || first == '@') {
       return true;
     }
-    if (first == '+' || first == '-') {
+    if (safeExpressions && (first == '+' || first == '-')) {
       return !FP_REGEX.matcher(entry.substring(1)).matches();
     }
     return false;
@@ -166,7 +287,7 @@ public final class CsvOutput {
   // quotes the entry
   private String quotedEntry(String entry) {
     StringBuilder buf = new StringBuilder(entry.length() + 8);
-    if (isExpressionPrefix(entry)) {
+    if (safeExpressions && isExpressionPrefix(entry)) {
       buf.append('=');
     }
     buf.append('"');
