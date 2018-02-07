d5ab43f89e1f6d6282c36ef49e4cbae74ec9ba0e
Stephen Colebourne <scolebourne@joda.org>
2018-02-07 16:57:55 +0000

    Extend schedules to better handle 4 week frequency (#1652)
    Add extra check to avoid month-based logic to be run

--- modules/basics/src/main/java/com/opengamma/strata/basics/schedule/StubConvention.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/schedule/StubConvention.java
@@ -257,7 +257,7 @@ public enum StubConvention implements NamedEnum {
     ArgChecker.notNull(frequency, "frequency");
     // if the day-of-month differs, need to handle case where one or both
     // dates are at the end of the month, and in different months
-    if (this == NONE) {
+    if (this == NONE && frequency.isMonthBased()) {
       if (start.getDayOfMonth() != end.getDayOfMonth() &&
           start.getLong(PROLEPTIC_MONTH) != end.getLong(PROLEPTIC_MONTH) &&
           (start.getDayOfMonth() == start.lengthOfMonth() || end.getDayOfMonth() == end.lengthOfMonth())) {
