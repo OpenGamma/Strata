d5e3c4e2cda2afd167d95cb0130bca966b4eb99c
Stephen Colebourne <stephen@opengamma.com>
2018-02-20 12:55:39 +0000

    Add missing package-info

---
+++ modules/product/src/main/java/com/opengamma/strata/product/credit/type/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2018 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Conventions and templates to aid the construction of credit instruments.
+ */
+package com.opengamma.strata.product.credit.type;
