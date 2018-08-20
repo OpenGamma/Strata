34eb27ee90f3f4c3aa10677ca8edc11f25731d27
will <will@opengamma.com>
2018-08-20 15:22:22 +0100

    Formatting in test

--- modules/collect/src/test/java/com/opengamma/strata/collect/MapStreamTest.java
+++ modules/collect/src/test/java/com/opengamma/strata/collect/MapStreamTest.java
@@ -105,25 +105,6 @@ public class MapStreamTest {
     assertThat(result).isEqualTo(expected);
   }
 
-  public void flatMap() {
-    ImmutableMap<String, String> expected = ImmutableMap.<String, String>builder()
-        .put("one", "1")
-        .put("1", "one")
-        .put("two", "2")
-        .put("2", "two")
-        .put("three", "3")
-        .put("3", "three")
-        .put("four", "4")
-        .put("4", "four")
-        .build();
-
-    Map<String, String> result = MapStream.of(map)
-        .flatMap((k, v) -> Stream.of(Pair.of(k, Integer.toString(v)), Pair.of(Integer.toString(v), k)))
-        .collect(pairsToImmutableMap());
-
-    assertThat(result).isEqualTo(expected);
-  }
-
   public void flatMapKeysToKeys() {
     Map<String, Integer> expected = ImmutableMap.<String, Integer>builder()
         .put("one", 1)
@@ -196,6 +177,25 @@ public class MapStreamTest {
         .flatMapValues((key, value) -> Stream.of(key, Integer.toString(value)))
         .map((k, v) -> Pair.of(k, v))
         .collect(toList());
+
+    assertThat(result).isEqualTo(expected);
+  }
+
+  public void flatMap() {
+    Map<String, String> expected = ImmutableMap.<String, String>builder()
+        .put("one", "1")
+        .put("1", "one")
+        .put("two", "2")
+        .put("2", "two")
+        .put("three", "3")
+        .put("3", "three")
+        .put("four", "4")
+        .put("4", "four")
+        .build();
+
+    Map<String, String> result = MapStream.of(map)
+        .flatMap((k, v) -> Stream.of(Pair.of(k, Integer.toString(v)), Pair.of(Integer.toString(v), k)))
+        .collect(pairsToImmutableMap());
 
     assertThat(result).isEqualTo(expected);
   }
