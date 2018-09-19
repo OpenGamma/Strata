cd1ba37c43841d660f0be48230651dd278453d64
Richard <richard@opengamma.com>
2018-09-19 17:18:06 +0100

    Pricer for Americans with discrete dividends

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/impl/tree/TrinomialTree.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/impl/tree/TrinomialTree.java
@@ -66,6 +66,111 @@ public class TrinomialTree {
     return values.get(0);
   }
   
+  public double optionPrice(
+      OptionFunction function,
+      LatticeSpecification lattice,
+      double spot,
+      double volatility,
+      double interestRate,
+      double[] dividendYields,
+      double[] dividendTimes) {
+    
+    int nSteps = function.getNumberOfSteps();
+    double timeToExpiry = function.getTimeToExpiry();
+    double dt = timeToExpiry / (double) nSteps;
+    double discount = Math.exp(-interestRate * dt);
+    DoubleArray params = lattice.getParametersTrinomial(volatility, interestRate, dt);
+    double middleFactor = params.get(1);
+    double downFactor = params.get(2);
+    double upProbability = params.get(3);
+    double midProbability = params.get(4);
+    double downProbability = params.get(5);
+    double modifiedSpot = spot;
+    for(int i = 0; i < dividendTimes.length; ++i){
+      modifiedSpot *= 1 - dividendYields[i];
+    }
+    ArgChecker.isTrue(upProbability > 0d, "upProbability should be greater than 0");
+    ArgChecker.isTrue(upProbability < 1d, "upProbability should be smaller than 1");
+    ArgChecker.isTrue(midProbability > 0d, "midProbability should be greater than 0");
+    ArgChecker.isTrue(midProbability < 1d, "midProbability should be smaller than 1");
+    ArgChecker.isTrue(downProbability > 0d, "downProbability should be greater than 0");
+    DoubleArray values = function.getPayoffAtExpiryTrinomial(modifiedSpot, downFactor, middleFactor);
+    for (int i = nSteps - 1; i > -1; --i) {
+      double dividendMultiplier = 1;
+      for(int j = 0; j < dividendTimes.length; ++j){
+        if(i * dt > dividendTimes[j]){
+          dividendMultiplier *= (1 - dividendYields[j]);
+        }
+      }
+        
+      values = function.getNextOptionValues(discount, upProbability, midProbability, downProbability, values, spot * dividendMultiplier,
+          downFactor, middleFactor, i);
+    }
+    return values.get(0);
+  }
+  
+  public ValueDerivatives optionPriceAdjoint(
+      OptionFunction function,
+      LatticeSpecification lattice,
+      double spot,
+      double volatility,
+      double interestRate,    
+      double[] dividendYields,
+      double[] dividendTimes) {
+    
+    int nSteps = function.getNumberOfSteps();
+    double timeToExpiry = function.getTimeToExpiry();
+    double dt = timeToExpiry / (double) nSteps;
+    double discount = Math.exp(-interestRate * dt);
+    DoubleArray params = lattice.getParametersTrinomial(volatility, interestRate, dt);
+    double upFactor = params.get(0);
+    double middleFactor = params.get(1);
+    double downFactor = params.get(2);
+    double upProbability = params.get(3);
+    double midProbability = params.get(4);
+    double downProbability = params.get(5);  
+    double modifiedSpot = spot;
+    for(int i = 0; i < dividendTimes.length; ++i){
+      modifiedSpot *= 1 - dividendYields[i];
+    }
+    ArgChecker.isTrue(upProbability > 0d, "upProbability should be greater than 0");
+    ArgChecker.isTrue(upProbability < 1d, "upProbability should be smaller than 1");
+    ArgChecker.isTrue(midProbability > 0d, "midProbability should be greater than 0");
+    ArgChecker.isTrue(midProbability < 1d, "midProbability should be smaller than 1");
+    ArgChecker.isTrue(downProbability > 0d, "downProbability should be greater than 0");
+    DoubleArray values = function.getPayoffAtExpiryTrinomial(modifiedSpot, downFactor, middleFactor);
+    double delta = 0d;
+    double gamma = 0d;
+    double theta = 0d;
+    for (int i = nSteps - 1; i > -1; --i) {
+      double dividendMultiplier = 1;
+      for(int j = 0; j < dividendTimes.length; ++j){
+        if(i * dt > dividendTimes[j]){
+          dividendMultiplier *= (1 - dividendYields[j]);
+        }
+      }
+      values = function.getNextOptionValues(discount, upProbability, midProbability, downProbability, values, spot * dividendMultiplier,
+          downFactor, middleFactor, i);
+      //We're assuming here that no dividend drop happens at first step. Crude but fine for now.
+      if (i == 1) {
+        double d1 = (values.get(2) - values.get(1)) / (spot * upFactor - spot);
+        double d2 = (values.get(1) - values.get(0)) / (spot - spot * downFactor);
+        delta = 0.5 * (d1 + d2);
+        //Get theta and gamma
+        gamma = (d1 - d2)/(0.5 * spot * (upFactor - downFactor));
+        theta = values.get(1);
+      }
+    }
+    theta = (theta - values.get(0)) / dt / 365;
+    
+    //Perform bump analysis for 
+    double bumpVol = optionPrice(function, lattice, spot, volatility + BUMP, interestRate, dividendYields, dividendTimes);
+    double bumpRate = optionPrice(function, lattice, spot, volatility, interestRate + BUMP, dividendYields, dividendTimes);
+    double vega = (bumpVol - values.get(0)) / BUMP;
+    double rho = (bumpRate - values.get(0)) / BUMP;
+    return ValueDerivatives.of(values.get(0), DoubleArray.of(delta, vega, rho, theta, gamma));
+  }
+  
   public ValueDerivatives optionPriceAdjoint(
       OptionFunction function,
       LatticeSpecification lattice,
