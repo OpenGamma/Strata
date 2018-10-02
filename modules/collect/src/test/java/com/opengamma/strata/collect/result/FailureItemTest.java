94704678aec9ed647c97a2c78b831bab1b8fc621
Will Nicholson <will@opengamma.com>
2018-10-02 11:02:21 +0100

    Add FailureItem.withAttributes() (#1803)

--- modules/collect/src/test/java/com/opengamma/strata/collect/result/FailureItemTest.java
+++ modules/collect/src/test/java/com/opengamma/strata/collect/result/FailureItemTest.java
@@ -105,10 +105,24 @@ public class FailureItemTest {
     assertEquals(test.toString(), "INVALID: failure: error: java.lang.IllegalArgumentException: exmsg");
   }
 
-  public void test_withAttributes() {
+  public void test_withAttribute() {
     FailureItem test = FailureItem.of(FailureReason.INVALID, "my {one} {two} failure", "big", "bad");
     test = test.withAttribute("foo", "bar");
     assertEquals(test.getAttributes(), ImmutableMap.of("one", "big", "two", "bad", "foo", "bar"));
+    assertEquals(test.getReason(), FailureReason.INVALID);
+    assertEquals(test.getMessage(), "my big bad failure");
+    assertEquals(test.getCauseType().isPresent(), false);
+    assertEquals(test.getStackTrace().contains(".FailureItem.of("), false);
+    assertEquals(test.getStackTrace().contains(".Failure.of("), false);
+    assertEquals(test.getStackTrace().startsWith("com.opengamma.strata.collect.result.FailureItem: my big bad failure"), true);
+    assertEquals(test.getStackTrace().contains(".test_withAttribute("), true);
+    assertEquals(test.toString(), "INVALID: my big bad failure");
+  }
+
+  public void test_withAttributes() {
+    FailureItem test = FailureItem.of(FailureReason.INVALID, "my {one} {two} failure", "big", "bad");
+    test = test.withAttributes(ImmutableMap.of("foo", "bar", "two", "good"));
+    assertEquals(test.getAttributes(), ImmutableMap.of("one", "big", "two", "good", "foo", "bar"));
     assertEquals(test.getReason(), FailureReason.INVALID);
     assertEquals(test.getMessage(), "my big bad failure");
     assertEquals(test.getCauseType().isPresent(), false);
