d3d0ad463e6fb6c047a653214cfccbed15d5f3b6
Will Nicholson <will@opengamma.com>
2018-10-19 14:14:28 +0100

    Forgotten import

--- modules/collect/src/main/java/com/opengamma/strata/collect/MapStream.java
+++ modules/collect/src/main/java/com/opengamma/strata/collect/MapStream.java
@@ -21,6 +21,7 @@ import java.util.Optional;
 import java.util.Spliterator;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
