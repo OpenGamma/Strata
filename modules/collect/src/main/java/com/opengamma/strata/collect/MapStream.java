11e7c9bdb2804fb6d5f5407f2e0e8563c76fd97f
Will Nicholson <will@opengamma.com>
2018-10-19 14:47:56 +0100

    MapStream extensions - anyMatch, allMatch, noneMatch with BiPredicate (#1832)
    * MapStream extensions - anyMatch, allMatch, noneMatch with BiPredicate
    
    * Forgotten import

--- modules/collect/src/main/java/com/opengamma/strata/collect/MapStream.java
+++ modules/collect/src/main/java/com/opengamma/strata/collect/MapStream.java
@@ -21,6 +21,7 @@ import java.util.Optional;
 import java.util.Spliterator;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -544,6 +545,44 @@ public final class MapStream<K, V>
    */
   public Optional<Map.Entry<K, V>> maxValues(Comparator<? super V> comparator) {
     return underlying.max((e1, e2) -> comparator.compare(e1.getValue(), e2.getValue()));
+  }
+
+  //-----------------------------------------------------------------------
+  /**
+   * Returns whether any elements of this stream match the provided predicate.
+   * <p>
+   * This is a short-circuiting terminal operation.
+   *
+   * @param predicate  the predicate to apply to the entries
+   * @return whether any of the entries matched the predicate
+   */
+  public boolean anyMatch(BiPredicate<? super K, ? super V> predicate) {
+    return underlying.anyMatch(e -> predicate.test(e.getKey(), e.getValue()));
+  }
+
+
+  /**
+   * Returns whether all elements of this stream match the provided predicate.
+   * <p>
+   * This is a short-circuiting terminal operation.
+   *
+   * @param predicate  the predicate to apply to the entries
+   * @return whether all of the entries matched the predicate
+   */
+  public boolean allMatch(BiPredicate<? super K, ? super V> predicate) {
+    return underlying.allMatch(e -> predicate.test(e.getKey(), e.getValue()));
+  }
+
+  /**
+   * Returns whether no elements of this stream match the provided predicate.
+   * <p>
+   * This is a short-circuiting terminal operation.
+   *
+   * @param predicate  the predicate to apply to the entries
+   * @return whether none of the entries matched the predicate
+   */
+  public boolean noneMatch(BiPredicate<? super K, ? super V> predicate) {
+    return underlying.noneMatch(e -> predicate.test(e.getKey(), e.getValue()));
   }
 
   //-------------------------------------------------------------------------
