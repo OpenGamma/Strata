c7e34d5b98b94677653e173bc5a32a8d3665e8a5
Richard <richard@opengamma.com>
2018-10-24 20:24:23 +0100

    Initial par Rate sensitivities

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/swap/DiscountingSwapProductPricer.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/swap/DiscountingSwapProductPricer.java
@@ -358,6 +358,29 @@ public class DiscountingSwapProductPricer {
         otherLegsConvertedPv += (pvLocal * provider.fxRate(leg.getCurrency(), ccyFixedLeg));
       }
     }
+    SwapPaymentPeriod firstPeriod = fixedLeg.getPaymentPeriods().get(0);
+    if (firstPeriod instanceof RatePaymentPeriod) {
+      RatePaymentPeriod payment = (RatePaymentPeriod) firstPeriod;
+      RateAccrualPeriod firstAccrualPeriod = payment.getAccrualPeriods().get(0);
+      if (firstAccrualPeriod.getRateComputation() instanceof FixedOvernightCompoundedAnnualRateComputation) {
+        double notional = payment.getNotional();
+        double df = provider.discountFactor(ccyFixedLeg, payment.getPaymentDate());
+        double af = firstAccrualPeriod.getYearFraction();
+        double otherLegsConvertedPvBar = -Math.pow(-otherLegsConvertedPv  / (notional * df) + 1.0d, 1.0 / af - 1.0d) / (af * notional * df);
+        double dfBar = Math.pow(-otherLegsConvertedPv  / (notional * df) + 1.0d, 1.0 / af - 1.0d) * otherLegsConvertedPv / (af * notional * df * df);
+        PointSensitivityBuilder otherLegsConvertedPvDr = PointSensitivityBuilder.none();
+        for (ResolvedSwapLeg leg : swap.getLegs()) {
+          if (leg != fixedLeg) {
+            PointSensitivityBuilder pvLegDr = getLegPricer().presentValueSensitivity(leg, provider)
+                .multipliedBy(provider.fxRate(leg.getCurrency(), ccyFixedLeg));
+            otherLegsConvertedPvDr = otherLegsConvertedPvDr.combinedWith(pvLegDr);
+          }
+        }
+        otherLegsConvertedPvDr = otherLegsConvertedPvDr.withCurrency(ccyFixedLeg);
+        PointSensitivityBuilder dfDr = provider.discountFactors(ccyFixedLeg).zeroRatePointSensitivity(firstPeriod.getPaymentDate());
+        return dfDr.multipliedBy(dfBar).combinedWith(otherLegsConvertedPvDr.multipliedBy(otherLegsConvertedPvBar));
+      }
+    }
     double fixedLegEventsPv = legPricer.presentValueEventsInternal(fixedLeg, provider);
     double pvbpFixedLeg = legPricer.pvbp(fixedLeg, provider);
     // Backward sweep
