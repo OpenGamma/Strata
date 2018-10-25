40d4ec45cfac909b9d439b152429dbb528dd0c25
Richard <richard@opengamma.com>
2018-10-25 13:30:16 +0100

    Test for Fixed rate swap convention now accepts a future value notional

--- modules/product/src/test/java/com/opengamma/strata/product/swap/type/FixedRateSwapLegConventionTest.java
+++ modules/product/src/test/java/com/opengamma/strata/product/swap/type/FixedRateSwapLegConventionTest.java
@@ -31,8 +31,10 @@ import com.opengamma.strata.basics.date.DaysAdjustment;
 import com.opengamma.strata.basics.schedule.PeriodicSchedule;
 import com.opengamma.strata.basics.schedule.RollConventions;
 import com.opengamma.strata.basics.schedule.StubConvention;
+import com.opengamma.strata.basics.value.ValueSchedule;
 import com.opengamma.strata.product.swap.CompoundingMethod;
 import com.opengamma.strata.product.swap.FixedRateCalculation;
+import com.opengamma.strata.product.swap.FutureValueNotional;
 import com.opengamma.strata.product.swap.NotionalSchedule;
 import com.opengamma.strata.product.swap.PaymentSchedule;
 import com.opengamma.strata.product.swap.RateCalculationSwapLeg;
@@ -121,8 +123,8 @@ public class FixedRateSwapLegConventionTest {
     FixedRateSwapLegConvention base = FixedRateSwapLegConvention.of(GBP, ACT_365F, P3M, BDA_MOD_FOLLOW);
     LocalDate startDate = LocalDate.of(2015, 5, 5);
     LocalDate endDate = LocalDate.of(2020, 5, 5);
-    RateCalculationSwapLeg test = base.toLeg(startDate, endDate, PAY, NOTIONAL_2M, 0.25d);
-    RateCalculationSwapLeg expected = RateCalculationSwapLeg.builder()
+    RateCalculationSwapLeg test1 = base.toLeg(startDate, endDate, PAY, NOTIONAL_2M, 0.25d);
+    RateCalculationSwapLeg expected1 = RateCalculationSwapLeg.builder()
         .payReceive(PAY)
         .accrualSchedule(PeriodicSchedule.builder()
             .frequency(P3M)
@@ -138,7 +140,30 @@ public class FixedRateSwapLegConventionTest {
         .notionalSchedule(NotionalSchedule.of(GBP, NOTIONAL_2M))
         .calculation(FixedRateCalculation.of(0.25d, ACT_365F))
         .build();
-    assertEquals(test, expected);
+    assertEquals(test1, expected1);
+
+    RateCalculationSwapLeg test2 = base.toLeg(startDate, endDate, PAY, NOTIONAL_2M, 0.25d, FutureValueNotional.auto());
+    RateCalculationSwapLeg expected2 = RateCalculationSwapLeg.builder()
+        .payReceive(PAY)
+        .accrualSchedule(PeriodicSchedule.builder()
+            .frequency(P3M)
+            .startDate(startDate)
+            .endDate(endDate)
+            .businessDayAdjustment(BDA_MOD_FOLLOW)
+            .stubConvention(StubConvention.SHORT_INITIAL)
+            .build())
+        .paymentSchedule(PaymentSchedule.builder()
+            .paymentFrequency(P3M)
+            .paymentDateOffset(DaysAdjustment.NONE)
+            .build())
+        .notionalSchedule(NotionalSchedule.of(GBP, NOTIONAL_2M))
+        .calculation(FixedRateCalculation.builder()
+            .rate(ValueSchedule.of(0.25d))
+            .dayCount(ACT_365F)
+            .futureValueNotional(FutureValueNotional.auto())
+            .build())
+        .build();
+    assertEquals(test2, expected2);
   }
 
   //-------------------------------------------------------------------------
