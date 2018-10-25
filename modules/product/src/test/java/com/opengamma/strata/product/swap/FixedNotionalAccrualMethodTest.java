d09244ff3827cbe50099e33f2ea8a9e72d74c9c8
Richard <richard@opengamma.com>
2018-10-25 17:37:52 +0100

    Enum for fixed accrual method

--- modules/product/src/test/java/com/opengamma/strata/product/swap/FixedNotionalAccrualMethodTest.java
+++ modules/product/src/test/java/com/opengamma/strata/product/swap/FixedNotionalAccrualMethodTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Copyright (C) 2018 - present by OpenGamma Inc. and the OpenGamma group of companies
  *
  * Please see distribution for license.
  */
@@ -14,52 +14,45 @@ import static org.testng.Assert.assertEquals;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-/**
- * Test.
- */
-@Test
-public class OvernightAccrualMethodTest {
-
+public class FixedNotionalAccrualMethodTest {
+  
   //-------------------------------------------------------------------------
   @DataProvider(name = "name")
   public static Object[][] data_name() {
     return new Object[][] {
-        {OvernightAccrualMethod.AVERAGED, "Averaged"},
-        {OvernightAccrualMethod.COMPOUNDED, "Compounded"},
-        {OvernightAccrualMethod.OVERNIGHT_COMPOUNDED_ANNUAL_RATE, "OvernightCompoundedAnnualRate"},
-        {OvernightAccrualMethod.AVERAGED_DAILY, "AveragedDaily"},
+        {FixedNotionalAccrualMethod.NONE, "None"},
+        {FixedNotionalAccrualMethod.OVERNIGHT_COMPOUNDED_ANNUAL_RATE, "OvernightCompoundedAnnualRate"},
     };
   }
-
+  
   @Test(dataProvider = "name")
-  public void test_toString(OvernightAccrualMethod convention, String name) {
+  public void test_toString(FixedNotionalAccrualMethod convention, String name) {
     assertEquals(convention.toString(), name);
   }
-
+  
   @Test(dataProvider = "name")
-  public void test_of_lookup(OvernightAccrualMethod convention, String name) {
-    assertEquals(OvernightAccrualMethod.of(name), convention);
+  public void test_of_lookup(FixedNotionalAccrualMethod convention, String name) {
+    assertEquals(FixedNotionalAccrualMethod.of(name), convention);
   }
-
+  
   public void test_of_lookup_notFound() {
-    assertThrows(() -> OvernightAccrualMethod.of("Rubbish"), IllegalArgumentException.class);
+    assertThrows(() -> FixedNotionalAccrualMethod.of("Rubbish"), IllegalArgumentException.class);
   }
-
+  
   public void test_of_lookup_null() {
-    assertThrows(() -> OvernightAccrualMethod.of(null), IllegalArgumentException.class);
+    assertThrows(() -> FixedNotionalAccrualMethod.of(null), IllegalArgumentException.class);
   }
-
+  
   //-------------------------------------------------------------------------
   public void coverage() {
-    coverEnum(OvernightAccrualMethod.class);
+    coverEnum(FixedNotionalAccrualMethod.class);
   }
-
+  
   public void test_serialization() {
-    assertSerialization(OvernightAccrualMethod.AVERAGED);
+    assertSerialization(FixedNotionalAccrualMethod.NONE);
   }
-
+  
   public void test_jodaConvert() {
-    assertJodaConvert(OvernightAccrualMethod.class, OvernightAccrualMethod.AVERAGED);
+    assertJodaConvert(FixedNotionalAccrualMethod.class, FixedNotionalAccrualMethod.NONE);
   }
-
 }
