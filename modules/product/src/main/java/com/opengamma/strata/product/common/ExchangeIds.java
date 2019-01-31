f53b3981c59c2796b46934a56dd85d28b62d4b75
Abdi <abdi@opengamma.com>
2019-01-31 14:57:06 +0000

    Add korean exch

--- modules/product/src/main/java/com/opengamma/strata/product/common/ExchangeIds.java
+++ modules/product/src/main/java/com/opengamma/strata/product/common/ExchangeIds.java
@@ -108,6 +108,9 @@ public final class ExchangeIds {
   /** Euronext Exchange - Paris MATIF */
   public static final ExchangeId XMAT = ExchangeId.of("XMAT");
 
+  /** Korea Exchange (Futures Market) */
+  public static final ExchangeId XKFE = ExchangeId.of("XKFE");
+
   //-------------------------------------------------------------------------
   /**
    * Restricted constructor.
