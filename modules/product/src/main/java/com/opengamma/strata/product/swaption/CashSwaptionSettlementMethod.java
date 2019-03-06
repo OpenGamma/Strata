649f4d1ecbdd0bd25376c08c89cc9afb19e854cf
Marc Henrard <marc-henrard@users.noreply.github.com>
2019-03-06 17:05:57 +0100

    New cash settlement method for swaptions: Collateralized cash price (#1902)
    * Added new cash settlement method for swaptions: Collateralized cash price
    
    * Corrected ISDA reference

--- modules/product/src/main/java/com/opengamma/strata/product/swaption/CashSwaptionSettlementMethod.java
+++ modules/product/src/main/java/com/opengamma/strata/product/swaption/CashSwaptionSettlementMethod.java
@@ -35,7 +35,15 @@ public enum CashSwaptionSettlementMethod implements NamedEnum {
    * <p>
    * The settlement amount is computed with the discount factor based on the agreed zero coupon curve.
    */
-  ZERO_COUPON_YIELD;
+  ZERO_COUPON_YIELD,
+  /**
+   * The Collateralized Cash Price
+   * <p>
+   * The settlement amount is computed  with the discount factor based where the parties are deemed to have a 
+   * bilateral, zero-threshold ISDA CSA. 
+   * Defined by the 2006 ISDA definitions 18.3g.
+   */
+  COLLATERALIZED_CASH_PRICE;
 
   // helper for name conversions
   private static final EnumNames<CashSwaptionSettlementMethod> NAMES = EnumNames.of(CashSwaptionSettlementMethod.class);
