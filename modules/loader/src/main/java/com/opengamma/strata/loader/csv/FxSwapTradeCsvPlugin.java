bdcf2e823c0502b1cea8bbe10d0b323ed82832c9
Stephen Colebourne <scolebourne@joda.org>
2019-04-16 14:17:10 +0100

    Parse FX vanilla option CSV (#1945)
    Also use new common utility methods
    Fixes #1785

--- modules/loader/src/main/java/com/opengamma/strata/loader/csv/FxSwapTradeCsvPlugin.java
+++ modules/loader/src/main/java/com/opengamma/strata/loader/csv/FxSwapTradeCsvPlugin.java
@@ -10,11 +10,11 @@ import static com.opengamma.strata.loader.csv.TradeCsvLoader.CONVENTION_FIELD;
 import static com.opengamma.strata.loader.csv.TradeCsvLoader.CURRENCY_FIELD;
 import static com.opengamma.strata.loader.csv.TradeCsvLoader.FX_RATE_FIELD;
 import static com.opengamma.strata.loader.csv.TradeCsvLoader.NOTIONAL_FIELD;
+import static com.opengamma.strata.loader.csv.TradeCsvLoader.PAYMENT_DATE_FIELD;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
-import com.opengamma.strata.basics.currency.Currency;
 import com.opengamma.strata.basics.currency.CurrencyAmount;
 import com.opengamma.strata.basics.currency.CurrencyPair;
 import com.opengamma.strata.basics.currency.FxRate;
@@ -24,7 +24,6 @@ import com.opengamma.strata.loader.LoaderUtils;
 import com.opengamma.strata.product.TradeInfo;
 import com.opengamma.strata.product.common.BuySell;
 import com.opengamma.strata.product.fx.FxSingle;
-import com.opengamma.strata.product.fx.FxSingleTrade;
 import com.opengamma.strata.product.fx.FxSwap;
 import com.opengamma.strata.product.fx.FxSwapTrade;
 
@@ -33,7 +32,6 @@ import com.opengamma.strata.product.fx.FxSwapTrade;
  */
 class FxSwapTradeCsvPlugin {
 
-  private static final String PAYMENT_DATE_FIELD = "Payment Date";
   private static final String FAR_FX_RATE_DATE_FIELD = "Far FX Rate";
   private static final String FAR_PAYMENT_DATE_FIELD = "Far Payment Date";
 
@@ -42,8 +40,8 @@ class FxSwapTradeCsvPlugin {
    *
    * @param row  the CSV row object
    * @param info  the trade info object
-   * @param resolver  the resolver used to parse additional information. This is not currently used in this method.
-   * @return the parsed trade, as an instance of {@link FxSingleTrade}
+   * @param resolver  the resolver used to parse additional information
+   * @return the parsed trade
    */
   static FxSwapTrade parse(CsvRow row, TradeInfo info, TradeCsvInfoResolver resolver) {
     FxSwapTrade trade = parseRow(row, info);
@@ -65,15 +63,13 @@ class FxSwapTradeCsvPlugin {
   private static FxSwapTrade parseConvention(CsvRow row, TradeInfo info) {
     CurrencyPair pair = CurrencyPair.parse(row.getValue(CONVENTION_FIELD));
     BuySell buySell = LoaderUtils.parseBuySell(row.getValue(BUY_SELL_FIELD));
-    Currency currency = Currency.parse(row.getValue(CURRENCY_FIELD));
-    double notional = LoaderUtils.parseDouble(row.getValue(NOTIONAL_FIELD));
+    CurrencyAmount amount = buySell.normalize(CsvLoaderUtils.parseCurrencyAmount(row, CURRENCY_FIELD, NOTIONAL_FIELD));
     double nearFxRate = LoaderUtils.parseDouble(row.getValue(FX_RATE_FIELD));
     double farFxRate = LoaderUtils.parseDouble(row.getValue(FAR_FX_RATE_DATE_FIELD));
     LocalDate nearPaymentDate = LoaderUtils.parseDate(row.getValue(PAYMENT_DATE_FIELD));
     LocalDate farPaymentDate = LoaderUtils.parseDate(row.getValue(FAR_PAYMENT_DATE_FIELD));
     Optional<BusinessDayAdjustment> paymentAdj = FxSingleTradeCsvPlugin.parsePaymentDateAdjustment(row);
 
-    CurrencyAmount amount = CurrencyAmount.of(currency, buySell.normalize(notional));
     FxRate nearRate = FxRate.of(pair, nearFxRate);
     FxRate farRate = FxRate.of(pair, farFxRate);
     FxSwap fx = paymentAdj
