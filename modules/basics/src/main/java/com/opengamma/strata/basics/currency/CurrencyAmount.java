1a8b5c3f9a31c957bc67be71e6629c9b21bc0126
Stephen Colebourne <stephen@opengamma.com>
2019-06-28 10:21:50 +0100

    Add comment about optimizations

--- modules/basics/src/main/java/com/opengamma/strata/basics/currency/CurrencyAmount.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/currency/CurrencyAmount.java
@@ -115,6 +115,7 @@ public final class CurrencyAmount
    */
   @FromString
   public static CurrencyAmount parse(String amountStr) {
+    // this method has had some performance optimizations applied
     if (amountStr == null || amountStr.length() <= 4 || amountStr.charAt(3) != ' ') {
       throw new IllegalArgumentException("Unable to parse amount, invalid format: " + amountStr);
     }
