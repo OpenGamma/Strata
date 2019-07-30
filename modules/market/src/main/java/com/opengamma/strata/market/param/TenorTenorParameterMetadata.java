b7e58b25dbe77d2b38bd8278f1c08f6c90e58d60
Stephen Colebourne <stephen@opengamma.com>
2019-07-30 18:20:08 +0100

    Simplify TenorTenorParameterMetadata (#2043)

--- modules/market/src/main/java/com/opengamma/strata/market/param/TenorTenorParameterMetadata.java
+++ modules/market/src/main/java/com/opengamma/strata/market/param/TenorTenorParameterMetadata.java
@@ -44,16 +44,6 @@ public final class TenorTenorParameterMetadata
   @PropertyDefinition(validate = "notNull")
   private final Tenor underlyingTenor;
   /**
-   * The expiry tenor year fraction.
-   */
-  @PropertyDefinition
-  private final double expiryYearFraction;
-  /**
-   * The underlying tenor year fraction.
-   */
-  @PropertyDefinition
-  private final double underlyingTenorYearFraction;
-  /**
    * The label that describes the parameter, defaulted to both tenors.
    */
   @PropertyDefinition(validate = "notEmpty", overrideGet = true)
@@ -66,23 +56,11 @@ public final class TenorTenorParameterMetadata
    *
    * @param expiryTenor the expiry tenor
    * @param underlyingTenor the underlying
-   * @param expiryTenorYearFraction the expiry tenor year fraction
-   * @param underlyingTenorYearFraction the underlying tenor year fraction
    * @return the metadata
    */
-  public static TenorTenorParameterMetadata of(
-      Tenor expiryTenor,
-      Tenor underlyingTenor,
-      double expiryTenorYearFraction,
-      double underlyingTenorYearFraction) {
-
+  public static TenorTenorParameterMetadata of(Tenor expiryTenor, Tenor underlyingTenor) {
     String label = Pair.of(expiryTenor, underlyingTenor).toString();
-    return new TenorTenorParameterMetadata(
-        expiryTenor,
-        underlyingTenor,
-        expiryTenorYearFraction,
-        underlyingTenorYearFraction,
-        label);
+    return new TenorTenorParameterMetadata(expiryTenor, underlyingTenor, label);
   }
 
   /**
@@ -90,24 +68,11 @@ public final class TenorTenorParameterMetadata
    *
    * @param expiryTenor the expiry tenor
    * @param underlyingTenor the underlying
-   * @param expiryTenorYearFraction the expiry tenor year fraction
-   * @param underlyingTenorYearFraction the underlying tenor year fraction
    * @param label the label
    * @return the metadata
    */
-  public static TenorTenorParameterMetadata of(
-      Tenor expiryTenor,
-      Tenor underlyingTenor,
-      double expiryTenorYearFraction,
-      double underlyingTenorYearFraction,
-      String label) {
-
-    return new TenorTenorParameterMetadata(
-        expiryTenor,
-        underlyingTenor,
-        expiryTenorYearFraction,
-        underlyingTenorYearFraction,
-        label);
+  public static TenorTenorParameterMetadata of(Tenor expiryTenor, Tenor underlyingTenor, String label) {
+    return new TenorTenorParameterMetadata(expiryTenor, underlyingTenor, label);
   }
 
   @ImmutablePreBuild
@@ -143,16 +108,12 @@ public final class TenorTenorParameterMetadata
   private TenorTenorParameterMetadata(
       Tenor expiryTenor,
       Tenor underlyingTenor,
-      double expiryYearFraction,
-      double underlyingTenorYearFraction,
       String label) {
     JodaBeanUtils.notNull(expiryTenor, "expiryTenor");
     JodaBeanUtils.notNull(underlyingTenor, "underlyingTenor");
     JodaBeanUtils.notEmpty(label, "label");
     this.expiryTenor = expiryTenor;
     this.underlyingTenor = underlyingTenor;
-    this.expiryYearFraction = expiryYearFraction;
-    this.underlyingTenorYearFraction = underlyingTenorYearFraction;
     this.label = label;
   }
 
@@ -181,24 +142,6 @@ public final class TenorTenorParameterMetadata
 
   //-----------------------------------------------------------------------
   /**
-   * Gets the expiry tenor year fraction.
-   * @return the value of the property
-   */
-  public double getExpiryYearFraction() {
-    return expiryYearFraction;
-  }
-
-  //-----------------------------------------------------------------------
-  /**
-   * Gets the underlying tenor year fraction.
-   * @return the value of the property
-   */
-  public double getUnderlyingTenorYearFraction() {
-    return underlyingTenorYearFraction;
-  }
-
-  //-----------------------------------------------------------------------
-  /**
    * Gets the label that describes the parameter, defaulted to both tenors.
    * @return the value of the property, not empty
    */
@@ -217,8 +160,6 @@ public final class TenorTenorParameterMetadata
       TenorTenorParameterMetadata other = (TenorTenorParameterMetadata) obj;
       return JodaBeanUtils.equal(expiryTenor, other.expiryTenor) &&
           JodaBeanUtils.equal(underlyingTenor, other.underlyingTenor) &&
-          JodaBeanUtils.equal(expiryYearFraction, other.expiryYearFraction) &&
-          JodaBeanUtils.equal(underlyingTenorYearFraction, other.underlyingTenorYearFraction) &&
           JodaBeanUtils.equal(label, other.label);
     }
     return false;
@@ -229,20 +170,16 @@ public final class TenorTenorParameterMetadata
     int hash = getClass().hashCode();
     hash = hash * 31 + JodaBeanUtils.hashCode(expiryTenor);
     hash = hash * 31 + JodaBeanUtils.hashCode(underlyingTenor);
-    hash = hash * 31 + JodaBeanUtils.hashCode(expiryYearFraction);
-    hash = hash * 31 + JodaBeanUtils.hashCode(underlyingTenorYearFraction);
     hash = hash * 31 + JodaBeanUtils.hashCode(label);
     return hash;
   }
 
   @Override
   public String toString() {
-    StringBuilder buf = new StringBuilder(192);
+    StringBuilder buf = new StringBuilder(128);
     buf.append("TenorTenorParameterMetadata{");
     buf.append("expiryTenor").append('=').append(expiryTenor).append(',').append(' ');
     buf.append("underlyingTenor").append('=').append(underlyingTenor).append(',').append(' ');
-    buf.append("expiryYearFraction").append('=').append(expiryYearFraction).append(',').append(' ');
-    buf.append("underlyingTenorYearFraction").append('=').append(underlyingTenorYearFraction).append(',').append(' ');
     buf.append("label").append('=').append(JodaBeanUtils.toString(label));
     buf.append('}');
     return buf.toString();
@@ -269,16 +206,6 @@ public final class TenorTenorParameterMetadata
     private final MetaProperty<Tenor> underlyingTenor = DirectMetaProperty.ofImmutable(
         this, "underlyingTenor", TenorTenorParameterMetadata.class, Tenor.class);
     /**
-     * The meta-property for the {@code expiryYearFraction} property.
-     */
-    private final MetaProperty<Double> expiryYearFraction = DirectMetaProperty.ofImmutable(
-        this, "expiryYearFraction", TenorTenorParameterMetadata.class, Double.TYPE);
-    /**
-     * The meta-property for the {@code underlyingTenorYearFraction} property.
-     */
-    private final MetaProperty<Double> underlyingTenorYearFraction = DirectMetaProperty.ofImmutable(
-        this, "underlyingTenorYearFraction", TenorTenorParameterMetadata.class, Double.TYPE);
-    /**
      * The meta-property for the {@code label} property.
      */
     private final MetaProperty<String> label = DirectMetaProperty.ofImmutable(
@@ -290,8 +217,6 @@ public final class TenorTenorParameterMetadata
         this, null,
         "expiryTenor",
         "underlyingTenor",
-        "expiryYearFraction",
-        "underlyingTenorYearFraction",
         "label");
 
     /**
@@ -307,10 +232,6 @@ public final class TenorTenorParameterMetadata
           return expiryTenor;
         case -824175261:  // underlyingTenor
           return underlyingTenor;
-        case -616415694:  // expiryYearFraction
-          return expiryYearFraction;
-        case 497454690:  // underlyingTenorYearFraction
-          return underlyingTenorYearFraction;
         case 102727412:  // label
           return label;
       }
@@ -350,22 +271,6 @@ public final class TenorTenorParameterMetadata
     }
 
     /**
-     * The meta-property for the {@code expiryYearFraction} property.
-     * @return the meta-property, not null
-     */
-    public MetaProperty<Double> expiryYearFraction() {
-      return expiryYearFraction;
-    }
-
-    /**
-     * The meta-property for the {@code underlyingTenorYearFraction} property.
-     * @return the meta-property, not null
-     */
-    public MetaProperty<Double> underlyingTenorYearFraction() {
-      return underlyingTenorYearFraction;
-    }
-
-    /**
      * The meta-property for the {@code label} property.
      * @return the meta-property, not null
      */
@@ -381,10 +286,6 @@ public final class TenorTenorParameterMetadata
           return ((TenorTenorParameterMetadata) bean).getExpiryTenor();
         case -824175261:  // underlyingTenor
           return ((TenorTenorParameterMetadata) bean).getUnderlyingTenor();
-        case -616415694:  // expiryYearFraction
-          return ((TenorTenorParameterMetadata) bean).getExpiryYearFraction();
-        case 497454690:  // underlyingTenorYearFraction
-          return ((TenorTenorParameterMetadata) bean).getUnderlyingTenorYearFraction();
         case 102727412:  // label
           return ((TenorTenorParameterMetadata) bean).getLabel();
       }
@@ -410,8 +311,6 @@ public final class TenorTenorParameterMetadata
 
     private Tenor expiryTenor;
     private Tenor underlyingTenor;
-    private double expiryYearFraction;
-    private double underlyingTenorYearFraction;
     private String label;
 
     /**
@@ -428,10 +327,6 @@ public final class TenorTenorParameterMetadata
           return expiryTenor;
         case -824175261:  // underlyingTenor
           return underlyingTenor;
-        case -616415694:  // expiryYearFraction
-          return expiryYearFraction;
-        case 497454690:  // underlyingTenorYearFraction
-          return underlyingTenorYearFraction;
         case 102727412:  // label
           return label;
         default:
@@ -448,12 +343,6 @@ public final class TenorTenorParameterMetadata
         case -824175261:  // underlyingTenor
           this.underlyingTenor = (Tenor) newValue;
           break;
-        case -616415694:  // expiryYearFraction
-          this.expiryYearFraction = (Double) newValue;
-          break;
-        case 497454690:  // underlyingTenorYearFraction
-          this.underlyingTenorYearFraction = (Double) newValue;
-          break;
         case 102727412:  // label
           this.label = (String) newValue;
           break;
@@ -469,20 +358,16 @@ public final class TenorTenorParameterMetadata
       return new TenorTenorParameterMetadata(
           expiryTenor,
           underlyingTenor,
-          expiryYearFraction,
-          underlyingTenorYearFraction,
           label);
     }
 
     //-----------------------------------------------------------------------
     @Override
     public String toString() {
-      StringBuilder buf = new StringBuilder(192);
+      StringBuilder buf = new StringBuilder(128);
       buf.append("TenorTenorParameterMetadata.Builder{");
       buf.append("expiryTenor").append('=').append(JodaBeanUtils.toString(expiryTenor)).append(',').append(' ');
       buf.append("underlyingTenor").append('=').append(JodaBeanUtils.toString(underlyingTenor)).append(',').append(' ');
-      buf.append("expiryYearFraction").append('=').append(JodaBeanUtils.toString(expiryYearFraction)).append(',').append(' ');
-      buf.append("underlyingTenorYearFraction").append('=').append(JodaBeanUtils.toString(underlyingTenorYearFraction)).append(',').append(' ');
       buf.append("label").append('=').append(JodaBeanUtils.toString(label));
       buf.append('}');
       return buf.toString();
