61613095316e9ee1edad1814a1b1d02496326968
unknown <yuki@opengamma.com>
2019-09-27 17:01:18 +0100

    another white space

--- modules/math/src/main/java/com/opengamma/strata/math/impl/cern/Bessel.java
+++ modules/math/src/main/java/com/opengamma/strata/math/impl/cern/Bessel.java
@@ -7,7 +7,7 @@
  * This code is copied from the original library from the `cern.jet.math` package.
  */
 /*
-Copyright © 1999 CERN - European Organization for Nuclear Research. 
+Copyright © 1999 CERN - European Organization for Nuclear Research.
 Permission to use, copy, modify, distribute and sell this software and its documentation for any purpose
 is hereby granted without fee, provided that the above copyright notice appear in all copies and
 that both that copyright notice and this permission notice appear in supporting documentation.
