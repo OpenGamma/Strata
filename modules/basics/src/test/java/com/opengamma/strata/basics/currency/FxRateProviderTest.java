4ea3823cbbf0a80a7254b02c777ba4f906635637
Stephen Colebourne <stephen@opengamma.com>
2019-10-30 13:54:36 +0000

    Simple implementations of FxRateProvider (#2104)

--- modules/basics/src/test/java/com/opengamma/strata/basics/currency/FxRateProviderTest.java
+++ modules/basics/src/test/java/com/opengamma/strata/basics/currency/FxRateProviderTest.java
@@ -8,6 +8,7 @@ package com.opengamma.strata.basics.currency;
 import static com.opengamma.strata.basics.currency.Currency.GBP;
 import static com.opengamma.strata.basics.currency.Currency.USD;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,20 @@ import org.junit.jupiter.api.Test;
  * Test {@link FxRateProvider}.
  */
 public class FxRateProviderTest {
+
+  @Test
+  public void noConversion() {
+    FxRateProvider test = FxRateProvider.noConversion();
+    assertThatIllegalArgumentException().isThrownBy(() -> test.fxRate(GBP, GBP));
+    assertThatIllegalArgumentException().isThrownBy(() -> test.fxRate(GBP, USD));
+  }
+
+  @Test
+  public void minimal() {
+    FxRateProvider test = FxRateProvider.minimal();
+    assertThat(test.fxRate(GBP, GBP)).isEqualTo(1d);
+    assertThatIllegalArgumentException().isThrownBy(() -> test.fxRate(GBP, USD));
+  }
 
   @Test
   public void emptyMatrixCanHandleTrivialRate() {
