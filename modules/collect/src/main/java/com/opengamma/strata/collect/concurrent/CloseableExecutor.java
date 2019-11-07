7fcc2a938e96e2f95c35069b6e66095c88c3b3bc
Ashley Heath <ashleyheath@users.noreply.github.com>
2019-11-07 10:21:10 +0000

    Add CloseableExecutor utility class.

---
+++ modules/collect/src/main/java/com/opengamma/strata/collect/concurrent/CloseableExecutor.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2019 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.collect.concurrent;
+
+import java.util.concurrent.ExecutorService;
+
+import com.opengamma.strata.collect.ArgChecker;
+import com.opengamma.strata.collect.Unchecked;
+
+/**
+ * AutoCloseable wrapper around an executor.
+ * <p>
+ * This shuts down the wrapped executor when it is closed.
+ */
+public class CloseableExecutor implements AutoCloseable {
+
+  private final ExecutorService executorService;
+
+  private CloseableExecutor(ExecutorService executorService) {
+    this.executorService = ArgChecker.notNull(executorService, "executorService");
+  }
+
+  /**
+   * Returns a closeable executor that wraps a passed in executor.
+   * <p>
+   * The passed in executor is shut down when the returned CloseableExecutor is closed.
+   *
+   * @param executor the executor to wrap
+   * @return a CloseableExecutor
+   */
+  public static CloseableExecutor of(ExecutorService executor) {
+    return new CloseableExecutor(executor);
+  }
+
+  @Override
+  public void close() {
+    Unchecked.wrap(executorService::shutdown);
+  }
+}
