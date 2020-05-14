5c38f267d30e8a0bea4b0b9aa85031989c208f65
Will Nicholson <will@opengamma.com>
2020-05-14 10:45:59 +0100

    CloseableExecutor wait for tasks to exit (#2157)

--- modules/collect/src/main/java/com/opengamma/strata/collect/concurrent/CloseableExecutor.java
+++ modules/collect/src/main/java/com/opengamma/strata/collect/concurrent/CloseableExecutor.java
@@ -5,7 +5,9 @@
  */
 package com.opengamma.strata.collect.concurrent;
 
+import java.time.Duration;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import com.opengamma.strata.collect.ArgChecker;
 import com.opengamma.strata.collect.Unchecked;
@@ -13,7 +15,7 @@ import com.opengamma.strata.collect.Unchecked;
 /**
  * AutoCloseable wrapper around an executor.
  * <p>
- * This shuts down the wrapped executor when it is closed.
+ * This shuts down the wrapped executor when it is closed, and can wait for tasks to exit.
  */
 public class CloseableExecutor implements AutoCloseable {
 
@@ -21,14 +23,21 @@ public class CloseableExecutor implements AutoCloseable {
    * The wrapped executor service.
    */
   private final ExecutorService executorService;
+  /**
+   * The duration to wait for tasks to terminate.
+   */
+  private final Duration duration;
 
   /**
    * Restricted constructor.
    *
    * @param executorService  the underlying executor service
+   * @param duration  the duration to wait
    */
-  private CloseableExecutor(ExecutorService executorService) {
+  private CloseableExecutor(ExecutorService executorService, Duration duration) {
     this.executorService = ArgChecker.notNull(executorService, "executorService");
+    this.duration = ArgChecker.notNull(duration, "duration");
+    ArgChecker.isFalse(duration.isNegative(), "Duration to wait must be positive or zero: {}", duration);
   }
 
   /**
@@ -40,11 +49,30 @@ public class CloseableExecutor implements AutoCloseable {
    * @return a CloseableExecutor
    */
   public static CloseableExecutor of(ExecutorService executor) {
-    return new CloseableExecutor(executor);
+    return new CloseableExecutor(executor, Duration.ZERO);
+  }
+
+  /**
+   * Returns a closeable executor that wraps a passed in executor.
+   * <p>
+   * The passed in executor is shut down when the returned CloseableExecutor is closed, and waits for the given 
+   * duration for tasks to finish.
+   *
+   * @param executor  the executor to wrap
+   * @param duration  the duration to wait for tasks to exit
+   * @return a CloseableExecutor
+   */
+  public static CloseableExecutor of(ExecutorService executor, Duration duration) {
+    return new CloseableExecutor(executor, duration);
   }
 
   @Override
   public void close() {
-    Unchecked.wrap(executorService::shutdown);
+    Unchecked.wrap(() -> {
+      executorService.shutdown();
+      if (!duration.isZero()) {
+        executorService.awaitTermination(duration.toMillis(), TimeUnit.MILLISECONDS);
+      }
+    });
   }
 }
--- modules/collect/src/test/java/com/opengamma/strata/collect/concurrent/CloseableExecutorTest.java
+++ modules/collect/src/test/java/com/opengamma/strata/collect/concurrent/CloseableExecutorTest.java
@@ -8,7 +8,9 @@ package com.opengamma.strata.collect.concurrent;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import java.time.Duration;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +26,15 @@ public class CloseableExecutorTest {
       // no-op
     }
     verify(mockExecutorService).shutdown();
+  }
+
+  @Test
+  public void testAutoCloseDuration() throws Exception {
+    ExecutorService mockExecutorService = mock(ExecutorService.class);
+    try (CloseableExecutor ignored = CloseableExecutor.of(mockExecutorService, Duration.ofSeconds(1))) {
+      // no-op
+    }
+    verify(mockExecutorService).shutdown();
+    verify(mockExecutorService).awaitTermination(1000, TimeUnit.MILLISECONDS);
   }
 }
