52c7d35ce4ebfcee66f66160a7777270f5aeaed1
Guillaume Santoro <santorog@users.noreply.github.com>
2022-10-10 18:09:47 +0200

    Extend Standard Ibor Future Specs Coverage

--- modules/product/src/main/java/com/opengamma/strata/product/index/type/StandardIborFutureContractSpecs.java
+++ modules/product/src/main/java/com/opengamma/strata/product/index/type/StandardIborFutureContractSpecs.java
@@ -5,12 +5,14 @@
  */
 package com.opengamma.strata.product.index.type;
 
+import static com.opengamma.strata.basics.date.DateSequences.MONTHLY_IMM;
 import static com.opengamma.strata.basics.date.DateSequences.QUARTERLY_IMM;
 import static com.opengamma.strata.basics.date.DateSequences.QUARTERLY_IMM_3_SERIAL;
 import static com.opengamma.strata.basics.date.DateSequences.QUARTERLY_IMM_6_SERIAL;
 import static com.opengamma.strata.basics.index.IborIndices.CHF_LIBOR_3M;
 import static com.opengamma.strata.basics.index.IborIndices.EUR_EURIBOR_3M;
 import static com.opengamma.strata.basics.index.IborIndices.GBP_LIBOR_3M;
+import static com.opengamma.strata.basics.index.IborIndices.USD_LIBOR_1M;
 import static com.opengamma.strata.basics.index.IborIndices.USD_LIBOR_3M;
 
 /**
@@ -56,6 +58,34 @@ final class StandardIborFutureContractSpecs {
           .name("EUR-EURIBOR-3M-IMM-ICE")
           .index(EUR_EURIBOR_3M)
           .dateSequence(QUARTERLY_IMM_6_SERIAL)
+          .notional(1_000_000d)
+          .build();
+
+
+  //-------------------------------------------------------------------------
+  /**
+   * The 'EUR-EURIBOR-3M-IMM-EUREX' contract.
+   * <p>
+   * The EUREX "FEU3" contract based on quarterly IMM dates, also known as "FEU3".
+   */
+  public static final IborFutureContractSpec EUR_EURIBOR_3M_IMM_EUREX =
+      ImmutableIborFutureContractSpec.builder()
+          .name("EUR-EURIBOR-3M-IMM-ICE")
+          .index(EUR_EURIBOR_3M)
+          .dateSequence(QUARTERLY_IMM_6_SERIAL) //TODO check
+          .notional(1_000_000d)
+          .build();
+  //-------------------------------------------------------------------------
+  /**
+   * The 'USD-LIBOR-1M-IMM-CME' contract.
+   * <p>
+   * The CME "GLB" contract based on monthly IMM dates.
+   */
+  public static final IborFutureContractSpec USD_LIBOR_1M_IMM_CME =
+      ImmutableIborFutureContractSpec.builder()
+          .name("USD-LIBOR-1M-IMM-CME")
+          .index(USD_LIBOR_1M)
+          .dateSequence(MONTHLY_IMM) //TODO check
           .notional(1_000_000d)
           .build();
 
