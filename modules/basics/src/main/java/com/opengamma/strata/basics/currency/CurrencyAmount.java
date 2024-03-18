1bfc2a7cf939ea83a241a3203642561ce568ae1c
Brian Weller <brianweller89@users.noreply.github.com>
2024-03-18 22:05:32 +0000

    Remove redundant check in CurrencyAmount.negated()
    This is already handled in the constructor

--- modules/basics/src/main/java/com/opengamma/strata/basics/currency/CurrencyAmount.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/currency/CurrencyAmount.java
@@ -315,7 +315,7 @@ public final class CurrencyAmount
    */
   public CurrencyAmount negated() {
     // Zero is treated as a special case to avoid creating -0.0 which produces surprising equality behaviour
-    return new CurrencyAmount(currency, amount == 0d ? 0d : -amount);
+    return new CurrencyAmount(currency, -amount);
   }
 
   /**
