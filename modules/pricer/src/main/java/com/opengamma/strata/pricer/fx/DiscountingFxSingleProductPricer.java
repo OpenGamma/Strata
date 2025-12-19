413dc7c6382876c8f3636e5c767c23c3a999107b
SaponeRolando <149769037+SaponeRolando@users.noreply.github.com>
2025-12-19 14:47:09 +0100

    PROD-52637: checkstyle

--- modules/pricer/src/main/java/com/opengamma/strata/pricer/fx/DiscountingFxSingleProductPricer.java
+++ modules/pricer/src/main/java/com/opengamma/strata/pricer/fx/DiscountingFxSingleProductPricer.java
@@ -56,8 +56,8 @@ public class DiscountingFxSingleProductPricer {
    */
   public MultiCurrencyAmount presentValue(ResolvedFxSingle fx, RatesProvider provider) {
   if (!provider.getValuationDate().isBefore(fx.getPaymentDate())) {
-      return MultiCurrencyAmount.empty();
-    }
+    return MultiCurrencyAmount.empty();
+  }
     CurrencyAmount pv1 = paymentPricer.presentValue(fx.getBaseCurrencyPayment(), provider);
     CurrencyAmount pv2 = paymentPricer.presentValue(fx.getCounterCurrencyPayment(), provider);
     return MultiCurrencyAmount.of(pv1, pv2);
